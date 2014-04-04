c05b043934caa8d2aec340cde280ccd042b9434a
arp28 <arp28@b933007e-b0d1-11e3-9120-db60e37ba295>
2014-04-04 12:24:34 +0000

    Break-out time-smoothing code into a point-wise kernel

--- shallow/SEQ/time_smooth.f90
+++ shallow/SEQ/time_smooth.f90
@@ -12,6 +12,8 @@ CONTAINS
 
   !===================================================
 
+  !> Initialise the time-smoothing module. Sets parameter
+  !! alpha that is used in the time-smooth kernel.
   SUBROUTINE time_smooth_init(alpha_tmp)
     IMPLICIT none
     REAL(KIND=8), INTENT(in) :: alpha_tmp
@@ -22,6 +24,8 @@ CONTAINS
 
   !===================================================
 
+  !> Manual implementation of code to invoke the time-smoothing
+  !! kernel
   SUBROUTINE manual_invoke_time_smooth(field, field_new, field_old)
     IMPLICIT none
     REAL(KIND=8), INTENT(in), DIMENSION(:,:) :: field
@@ -31,16 +35,33 @@ CONTAINS
     INTEGER :: i, j
     INTEGER :: idim1, idim2
     
+    ! Here we will query what should be field objects to get at
+    ! raw data.
     idim1 = SIZE(field, 1)
     idim2 = SIZE(field, 2)
 
+    ! Loop over 'columns'
     DO J=1,idim2
       DO I=1,idim1
-        field_old(I,J) = field(I,J)+ &
-             ALPHA*(field_new(I,J)-2.*field(I,J)+field_old(I,J))
+         CALL time_smooth_code(i,j,field,field_new,field_old)
       END DO
     END DO
 
   END SUBROUTINE manual_invoke_time_smooth
+
+  !===================================================
+
+  !> Kernel to smooth supplied field in time
+  SUBROUTINE time_smooth_code(i, j, field, field_new, field_old)
+    IMPLICIT none
+    INTEGER,      INTENT(in)                    :: i, j
+    REAL(KIND=8), INTENT(in),    DIMENSION(:,:) :: field
+    REAL(KIND=8), INTENT(in),    DIMENSION(:,:) :: field_new
+    REAL(KIND=8), INTENT(inout), DIMENSION(:,:) :: field_old
+
+    field_old(i,j) = field(i,j) + &
+         alpha*(field_new(i,j) - 2.*field(i,j) + field_old(i,j))
+
+  END SUBROUTINE time_smooth_code
 
 END MODULE time_smooth
