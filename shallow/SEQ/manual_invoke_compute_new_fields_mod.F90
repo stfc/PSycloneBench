e17794821f5a83f2cbc643cfedc2aefbebac509c
arp28 <arp28@b933007e-b0d1-11e3-9120-db60e37ba295>
2014-05-22 20:43:32 +0000

    Use new topology module to get loop limits in my manual invoke compute_new_fields(). Add module to Makefile.

--- shallow/SEQ/manual_invoke_compute_new_fields_mod.F90
+++ shallow/SEQ/manual_invoke_compute_new_fields_mod.F90
@@ -10,45 +10,42 @@ CONTAINS
   SUBROUTINE manual_invoke_compute_new_fields(unew, uold, &
                                               vnew, vold, &
                                               pnew, pold, &
-                                              z, cu, cv, h, tdt)
+                                              z, cufld, cvfld, hfld, tdt)
     USE compute_unew, ONLY: compute_unew_code
     USE compute_vnew, ONLY: compute_vnew_code
     USE compute_pnew, ONLY: compute_pnew_code
+    use topology_mod, ONLY: cu, cv, ct
     IMPLICIT none
     REAL(wp), INTENT(out), DIMENSION(:,:) :: unew, vnew, pnew
     REAL(wp), INTENT(in),  DIMENSION(:,:) :: uold, vold, pold
-    REAL(wp), INTENT(in),  DIMENSION(:,:) :: z, cu, cv, h
+    REAL(wp), INTENT(in),  DIMENSION(:,:) :: z, cufld, cvfld, hfld
     REAL(wp), INTENT(in) :: tdt
     ! Locals
     integer :: i, j
-    integer :: m, n
-
-    m = size(z,1) - 1
-    n = size(z,2) - 1
 
     !CALL manual_invoke_compute_unew(unew, uold,  z, cv, h, tdt)
-    DO J=1, N !SIZE(z, 2) - 1
-       DO I=2, M+1 !SIZE(z, 1)
+    DO J=cu%jstart, cu%jstop 
+       DO I=cu%istart, cu%istop
 
           CALL compute_unew_code(i, j, unew, uold, &
-                                 z, cv, h, tdt)
+                                 z, cvfld, hfld, tdt)
 
        END DO
     END DO
     !CALL manual_invoke_compute_vnew(vnew, vold,  z, cu, h, tdt)
-    DO J=2, N+1 !SIZE(z, 2)
-       DO I=1, M !SIZE(z, 1) - 1
+    DO J=cv%jstart, cv%jstop
+       DO I=cv%istart, cv%istop
 
           CALL compute_vnew_code(i, j, vnew, vold, &
-                                 z, cu, h, tdt)
+                                 z, cufld, hfld, tdt)
        END DO
     END DO
     !CALL manual_invoke_compute_pnew(pnew, pold, cu, cv,    tdt)
-    DO J=1, N !SIZE(z, 2) - 1
-       DO I=1, M !SIZE(z, 1) - 1
+    DO J=ct%jstart, ct%jstop
+       DO I=ct%istart, ct%istop
 
           CALL compute_pnew_code(i, j, pnew, pold, &
-                                 cu, cv, tdt)
+                                 cufld, cvfld, tdt)
        END DO
     END DO
 
