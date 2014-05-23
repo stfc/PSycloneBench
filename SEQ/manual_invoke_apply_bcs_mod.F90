b71379abcd2bc7a87fbd0e85e4e886b115f82d25
arp28 <arp28@b933007e-b0d1-11e3-9120-db60e37ba295>
2014-05-23 16:30:52 +0000

    Change merged invokes of bc routines to call the manual_invokes rather than the _code routines.

--- SEQ/manual_invoke_apply_bcs_mod.F90
+++ SEQ/manual_invoke_apply_bcs_mod.F90
@@ -11,46 +11,32 @@ CONTAINS
   !===================================================
 
   SUBROUTINE manual_invoke_apply_bcs_uvtf(ufield, vfield, tfield, ffield)
-    USE apply_bcs_cf, ONLY: apply_bcs_cf_code
-    USE apply_bcs_ct, ONLY: apply_bcs_ct_code
-    USE apply_bcs_cu, ONLY: apply_bcs_cu_code
-    USE apply_bcs_cv, ONLY: apply_bcs_cv_code
+    USE apply_bcs_cf, ONLY: manual_invoke_apply_bcs_cf
+    use apply_bcs_ct, only: manual_invoke_apply_bcs_ct
+    use apply_bcs_cu, only: manual_invoke_apply_bcs_cu
+    use apply_bcs_cv, only: manual_invoke_apply_bcs_cv
     IMPLICIT none
     REAL(wp), INTENT(inout), DIMENSION(:,:) :: ufield, vfield, tfield, ffield
-    ! Locals
-    integer :: m, n, mp1, np1
 
-    MP1 = SIZE(ufield, 1)
-    NP1 = SIZE(ufield, 2)
-    M = MP1 - 1
-    N = NP1 - 1
-
-    call apply_bcs_cu_code(n, mp1, np1, ufield)
-    call apply_bcs_ct_code(n, mp1, np1, tfield)
-    call apply_bcs_cv_code(m, mp1, np1, vfield)
-    call apply_bcs_cf_code(mp1, np1, ffield)
+    call manual_invoke_apply_bcs_cu(ufield)
+    call manual_invoke_apply_bcs_ct(tfield)
+    call manual_invoke_apply_bcs_cv(vfield)
+    call manual_invoke_apply_bcs_cf(ffield)
 
   END SUBROUTINE manual_invoke_apply_bcs_uvtf
 
   !===================================================
 
   subroutine manual_invoke_apply_bcs_uvt(ufield, vfield, tfield)
-    use apply_bcs_ct, only: apply_bcs_ct_code
-    use apply_bcs_cu, only: apply_bcs_cu_code
-    use apply_bcs_cv, only: apply_bcs_cv_code
+    use apply_bcs_ct, only: manual_invoke_apply_bcs_ct
+    use apply_bcs_cu, only: manual_invoke_apply_bcs_cu
+    use apply_bcs_cv, only: manual_invoke_apply_bcs_cv
     implicit none
     real(wp), intent(inout), dimension(:,:) :: ufield, vfield, tfield
-    ! Locals
-    integer :: m, n, mp1, np1
 
-    MP1 = SIZE(ufield, 1)
-    NP1 = SIZE(ufield, 2)
-    M = MP1 - 1
-    N = NP1 - 1
-
-    call apply_bcs_cu_code(n, mp1, np1, ufield)
-    call apply_bcs_cv_code(m, mp1, np1, vfield)
-    call apply_bcs_ct_code(n, mp1, np1, tfield)
+    call manual_invoke_apply_bcs_cu(ufield)
+    call manual_invoke_apply_bcs_cv(vfield)
+    call manual_invoke_apply_bcs_ct(tfield)
 
   end subroutine manual_invoke_apply_bcs_uvt
 
