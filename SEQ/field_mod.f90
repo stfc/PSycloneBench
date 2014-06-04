73fa04186b21fc2b9b72c6c6ffac3123eec9a57e
arp28 <arp28@b933007e-b0d1-11e3-9120-db60e37ba295>
2014-06-04 08:43:45 +0000

    Addition of external data src to field type. Work on comments

--- SEQ/field_mod.f90
+++ SEQ/field_mod.f90
@@ -10,13 +10,59 @@ module field_mod
   ! on which it lives? If the latter then we have to query
   ! the field to see what type of grid it's on.
 
-  TYPE :: grid_type
+  !> Labels for the different types of 'external' data source
+  integer, parameter :: INTERNAL= 0
+  integer, parameter :: REMOTE  = 1
+  integer, parameter :: COUPLER = 2
+  integer, parameter :: FILE    = 3
+
+  ! Apart from MPI/thread halos, the application of external
+  ! boundary conditions is algorithm-driven.
+  ! e.g. the use of periodic boundary conditions is a key
+  ! physical characteristic of the model. Therefore, I'm not
+  ! sure that we need to capture the vagaries of where data
+  ! might come from.
+
+  !> Base type to capture the information needed about
+  !! an external region. This is a region of the field that
+  !! is not computed but receives data from elsewhere.
+  type :: ExtRegionType
+     !> What type of source this external region has
+     integer      :: SrcType
+     !> Where in the field the data is to be put
+     type(region) :: dest     
+  end type ExtRegionType
+
+  !> Internal source of data - i.e. data is to be copied
+  !! from another part of this same field.
+  type, extends(ExtRegionType) :: InternalSourceType
+     !> Location of source data within the field
+     type(region) :: src
+  end type InternalSourceType
+
+  !> Remote source of data - envisioned as another MPI
+  !! process.
+  type, extends(ExtRegionType) :: RemoteSourceType
+     !> MPI rank of process that owns data
+     integer      :: iproc
+     !> Region of data required from remote process
+     type(region) :: src
+  end type RemoteSourceType
+
+  !> External region to be populated with data read from file
+  type, extends(ExtRegionType) :: FileSourceType
+     !> Unit number of file
+     integer            :: unit_no
+     !> Name of the field in the file to read
+     character(len=128) :: field_name
+  end type FileSourceType
+
+  type :: grid_type
      !> Total number of grid points
-     INTEGER :: npts
+     integer :: npts
+  end type grid_type
 
-  END type grid_type
-
-  TYPE, EXTENDS(grid_type) :: rquad_type
+  type, extends(grid_type) :: rquad_type
      !> Extent of grid in x
      INTEGER :: nx
      !> Extent of grid in y
@@ -25,44 +71,41 @@ module field_mod
      REAL(wp) :: dx
      !> Grid spacing in y
      REAL(wp) :: dy
+  end type rquad_type
 
-  END TYPE rquad_type
+  type :: field_type
+     !> The grid on which this field is defined
+     type(rquad_type) :: grid
+     !> The number of external regions that this field has
+     integer :: NumExtRegions
+     !> Array of external regions
+     class(ExtRegionType), allocatable, dimension(:) :: ExtRegion
+     !> The internal region of this field
+     type(region) :: IntRegion
+  end type field_type
 
-  TYPE, EXTENDS(rquad_type) :: cgrid_type
+  type, extends(field_type) :: scalar_field_type
+     real(wp) :: data
+  end TYPE scalar_field_type
 
-     REAL(wp), DIMENSION(:,:), ALLOCATABLE :: data
+  type, extends(field_type) :: r2d_field_type
+     !> Array holding the actual field values
+     real(wp), dimension(:,:), allocatable :: data
+  end type r2d_field_type
 
-  END type cgrid_type
+  interface set
+     module procedure set_scalar_field
+  end interface set
 
-  TYPE, EXTENDS(cgrid_type) :: tpt_type
+  interface copy_field
+     module procedure copy_scalar_field, copy_2dfield, copy_2dfield_patch
+  end interface copy_field
 
-  END TYPE tpt_type
+  interface increment
+     module procedure increment_scalar_field
+  end interface increment
 
-  TYPE :: field_type
-     TYPE(grid_type) :: grid
-  END type field_type
-
-  TYPE, EXTENDS(field_type) :: scalar_field_type
-     REAL(wp) :: data
-  END TYPE scalar_field_type
-
-  TYPE, EXTENDS(field_type) :: r2d_field_type
-     REAL(wp), DIMENSION(:,:), ALLOCATABLE :: data
-  END type r2d_field_type
-
-  INTERFACE set
-     MODULE PROCEDURE set_scalar_field
-  END INTERFACE set
-
-  INTERFACE copy_field
-     MODULE PROCEDURE copy_scalar_field, copy_2dfield, copy_2dfield_patch
-  END INTERFACE copy_field
-
-  INTERFACE increment
-     MODULE PROCEDURE increment_scalar_field
-  END INTERFACE increment
-
-CONTAINS
+contains
 
   !===================================================
 
@@ -122,9 +165,17 @@ CONTAINS
 
   SUBROUTINE set_scalar_field(fld, val)
     IMPLICIT none
-    TYPE(scalar_field_type), INTENT(out) :: fld
+    class(field_type), INTENT(out) :: fld
     REAL(wp), INTENT(in) :: val
-    fld%data = val
+
+    select type(fld)
+    type is (scalar_field_type)
+       fld%data = val
+    type is (r2d_field_type)
+       fld%data = val
+    class default
+    end select
+
   END SUBROUTINE set_scalar_field
 
   !===================================================
@@ -135,4 +186,28 @@ CONTAINS
 !    REAL(wp), INTENT(in) :: val
 !
 !    fld%data = val
-END MODULE field_mod
+
+  !===================================================
+
+  subroutine update_external_regions(fld)
+    implicit none
+    class(field_type), intent(inout) :: fld
+    ! Locals
+    integer :: iregion
+
+    do iregion=1, fld%NumExtRegions, 1
+       write(*,*) 'External region ',iregion,' has type: ',&
+                  fld%ExtRegion(iregion)%SrcType
+
+       select case (fld%ExtRegion(iregion)%SrcType)
+       case (INTERNAL)
+       case (REMOTE)
+       case (COUPLER)
+       case (FILE)
+       case default
+       end select
+    end do
+
+  end subroutine update_external_regions
+
+end module field_mod
