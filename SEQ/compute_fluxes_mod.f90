b864f1538eeda39d0660b6eb6750d4daeb77549c
arp28 <arp28@b933007e-b0d1-11e3-9120-db60e37ba295>
2014-09-04 12:43:45 +0000

    Use M and N as loop limits in compute_fluxes_mod

--- SEQ/compute_fluxes_mod.f90
+++ SEQ/compute_fluxes_mod.f90
@@ -1,6 +1,5 @@
 module compute_fluxes_mod
   use kind_params_mod
-  use topology_mod, only: cu, cv, ct, cf
   use compute_cu_mod, only: compute_cu_code
   use compute_cv_mod, only: compute_cv_code
   use compute_z_mod, only: compute_z_code
@@ -16,6 +15,7 @@ contains
 
   subroutine invoke_compute_fluxes(cufld, cvfld, zfld, hfld, &
                                    pfld, ufld, vfld)
+    use topology_mod, only: M, N
     implicit none
     real(wp), intent(inout), dimension(:,:) :: cufld, cvfld, zfld, hfld
     real(wp), intent(in),    dimension(:,:) :: pfld, ufld, vfld
@@ -26,32 +26,32 @@ contains
     ! Change to hard-code lower-loop bounds with variable
     ! trip count expressed in terms of M/N+/-1
     ! M/N obtained from topology  look-up
-    do J=cu%jstart, cu%jstop
-       do I=cu%istart, cu%istop
+    do J= 1, N, 1
+       do I = 2, M+1, 1
 
           call compute_cu_code(i, j, cufld, pfld, ufld)
        end do
     end do
 
     !CALL invoke_compute_cv(CV, P, V)
-    do J=cv%jstart, cv%jstop !2, size(cv, 2)
-       do I=cv%istart, cv%istop !1, size(cv, 1) - 1
+    do J= 2, N+1, 1
+       do I= 1, M, 1
 
           call compute_cv_code(i, j, cvfld, pfld, vfld)
        end do
     end do
 
     !CALL invoke_compute_z(z, P, U, V)
-    do J=cf%jstart, cf%jstop !2, size(z, 2)
-       do I=cf%istart, cf%istop !2, size(z, 1)
+    do J= 2, N+1, 1
+       do I= 2, M+1, 1
 
           call compute_z_code(i, j, zfld, pfld, ufld, vfld)
        end do
     end do
 
     !CALL invoke_compute_h(h, P, U, V)
-    DO J=ct%jstart, ct%jstop !1, SIZE(h, 2) - 1
-       DO I=ct%istart, ct%istop !1, SIZE(h, 1) - 1
+    DO J= 1, N, 1
+       DO I= 1, M, 1
 
           CALL compute_h_code(i, j, hfld, pfld, ufld, vfld)
        END DO
