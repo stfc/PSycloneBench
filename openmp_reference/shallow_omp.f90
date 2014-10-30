15ae64ba0873481f15220e035a6677e817976708
arp28 <arp28@b933007e-b0d1-11e3-9120-db60e37ba295>
2014-10-30 12:22:22 +0000

    Working Fortran version of Grahams OpenMP C version of shallow

--- openmp_reference/shallow_omp.f90
+++ openmp_reference/shallow_omp.f90
@@ -75,15 +75,10 @@ PROGRAM shallow
   !> Integer tags for timers
   INTEGER :: idxt0, idxt1
 
-  INTEGER :: nthreads, chunk_size
+  INTEGER :: nthreads, chunk_size, myid
 
   !  ** Initialisations ** 
   CALL timer_init()
-
-  ! OpenMP set-up
-  nthreads = 1;
-!$ nthreads = omp_get_max_threads();
-  chunk_size = ceiling( real(M) / real(nthreads) )
 
   !     Read in namelist 
   OPEN(unit=input_unit, file=nml_name, status='old',iostat=ierr)
@@ -96,6 +91,11 @@ PROGRAM shallow
   !     Set up arrays
   m_len = m+1
   n_len = n+1
+
+  ! OpenMP set-up
+  nthreads = 1;
+!$ nthreads = omp_get_max_threads();
+  chunk_size = ceiling( real(M) / real(nthreads) )
 
   ALLOCATE( u(m_len,n_len), v(m_len,n_len), p(m_len,n_len) ) 
   ALLOCATE( unew(m_len,n_len), vnew(m_len,n_len), pnew(m_len,n_len) ) 
@@ -197,54 +197,93 @@ PROGRAM shallow
      call my_ncwrite(ncid,v_id,istart,icount,v(1:m,1:n),m,n,t_id,t_val)
   ENDIF
 
+  FSDX = 4./DX
+  FSDY = 4./DY
+
   !     Start timer
   TIME = 0.
 
   !     Start timer
   CALL timer_start('Time-stepping',idxt0)
 
+!$OMP PARALLEL default(none), &
+!$OMP         shared(M, N, MP1, NP1, itmax, fsdx, fsdy, dx, dy, cu, cv,       &
+!$OMP                u, v, z, h, p, uold, vold, pold, unew, vnew, pnew,       &
+!$OMP                time, dt, alpha, l_out, mprint, ptime, mnmin, istart,    &
+!$oMP                t_val, t_id, p_id, u_id, v_id, ncid, icount, chunk_size),&
+!$OMP          private(i, j, ncycle, tdts8, tdtsdx, tdtsdy, idxt1, myid),     &
+!$OMP          firstprivate(tdt)
+
+  myid = 0
+!$  myid = omp_get_thread_num()
+
   !  ** Start of time loop ** 
   DO ncycle=1,itmax
 
      !        COMPUTE CAPITAL U, CAPITAL V, Z AND H
-     FSDX = 4./DX
-     FSDY = 4./DY
 
      CALL timer_start('Compute CU,CV,CZ,H',idxt1)
 
+!$OMP DO SCHEDULE(static,chunk_size)
      DO J=1,N
         DO I=1,M
            CU(I+1,J) = .5*(P(I+1,J)+P(I,J))*U(I+1,J)
+        END DO
+     END DO
+!$OMP END DO nowait
+!$OMP DO SCHEDULE(static,chunk_size)
+     DO J=1,N
+        DO I=1,M
            CV(I,J+1) = .5*(P(I,J+1)+P(I,J))*V(I,J+1)
+        END DO
+     END DO
+!$OMP END DO nowait
+!$OMP DO SCHEDULE(static,chunk_size)
+     DO J=1,N
+        DO I=1,M
            Z(I+1,J+1) =(FSDX*(V(I+1,J+1)-V(I,J+1))-FSDY*(U(I+1,J+1) & 
                 -U(I+1,J)))/(P(I,J)+P(I+1,J)+P(I+1,J+1)+P(I,J+1))
+        END DO
+     END DO
+!$OMP END DO nowait
+!$OMP DO SCHEDULE(static,chunk_size)
+     DO J=1,N
+        DO I=1,M
            H(I,J) = P(I,J)+.25*(U(I+1,J)*U(I+1,J)+U(I,J)*U(I,J)     & 
                 +V(I,J+1)*V(I,J+1)+V(I,J)*V(I,J))
         END DO
      END DO
+!$OMP END DO nowait
 
      CALL timer_stop(idxt1)
+!$OMP BARRIER
 
-     CALL timer_start('PBCs',idxt1)
      !        PERIODIC CONTINUATION
+     CALL timer_start('PBCs',idxt1)
+!$OMP SINGLE
      DO J=1,N
         CU(1,J) = CU(M+1,J)
         CV(M+1,J+1) = CV(1,J+1)
         Z(1,J+1) = Z(M+1,J+1)
         H(M+1,J) = H(1,J)
      END DO
+     CU(1,N+1) = CU(M+1,1)
+     CV(M+1,1) = CV(1,N+1)
+     Z(1,1) = Z(M+1,N+1)
+     H(M+1,N+1) = H(1,1)
+!$OMP END SINGLE
+
+!$OMP DO SCHEDULE(static,chunk_size)
      DO I=1,M
         CU(I+1,N+1) = CU(I+1,1)
         CV(I,1) = CV(I,N+1)
         Z(I+1,1) = Z(I+1,N+1)
         H(I,N+1) = H(I,1)
      END DO
-     CU(1,N+1) = CU(M+1,1)
-     CV(M+1,1) = CV(1,N+1)
-     Z(1,1) = Z(M+1,N+1)
-     H(M+1,N+1) = H(1,1)
+!$OMP END DO nowait
 
      CALL timer_stop(idxt1)
+!$OMP BARRIER
 
      !        COMPUTE NEW VALUES U,V AND P
      TDTS8 = TDT/8.
@@ -253,86 +292,119 @@ PROGRAM shallow
 
      CALL timer_start('Compute {U,V,P}NEW',idxt1)
 
+!$OMP DO SCHEDULE(static,chunk_size)
      DO J=1,N
         DO I=1,M
            UNEW(I+1,J) = UOLD(I+1,J)+                                     &
                 TDTS8*(Z(I+1,J+1)+Z(I+1,J))*(CV(I+1,J+1)+CV(I,J+1)+CV(I,J) &
                 +CV(I+1,J))-TDTSDX*(H(I+1,J)-H(I,J))                       
-           VNEW(I,J+1) = VOLD(I,J+1)-TDTS8*(Z(I+1,J+1)+Z(I,J+1)) & 
+        END DO
+     END DO
+!$OMP END DO nowait
+!$OMP DO SCHEDULE(static,chunk_size)
+     DO J=1,N
+        DO I=1,M
+          VNEW(I,J+1) = VOLD(I,J+1)-TDTS8*(Z(I+1,J+1)+Z(I,J+1)) & 
                 *(CU(I+1,J+1)+CU(I,J+1)+CU(I,J)+CU(I+1,J))        & 
                 -TDTSDY*(H(I,J+1)-H(I,J))
+        END DO
+     END DO
+!$OMP END DO nowait
+!$OMP DO SCHEDULE(static,chunk_size)
+     DO J=1,N
+        DO I=1,M
            PNEW(I,J) = POLD(I,J)-TDTSDX*(CU(I+1,J)-CU(I,J))   & 
                 -TDTSDY*(CV(I,J+1)-CV(I,J))
         END DO
      END DO
+!$OMP END DO nowait
 
      CALL timer_stop(idxt1)
+!$OMP BARRIER
 
      CALL timer_start('PBCs',idxt1)
      !        PERIODIC CONTINUATION
+!$OMP SINGLE
      DO J=1,N
         UNEW(1,J) = UNEW(M+1,J)
         VNEW(M+1,J+1) = VNEW(1,J+1)
         PNEW(M+1,J) = PNEW(1,J)
      END DO
+     UNEW(1,N+1) = UNEW(M+1,1)
+     VNEW(M+1,1) = VNEW(1,N+1)
+     PNEW(M+1,N+1) = PNEW(1,1)
+!$OMP END SINGLE
+
+!$OMP DO SCHEDULE(static,chunk_size)
      DO I=1,M
         UNEW(I+1,N+1) = UNEW(I+1,1)
         VNEW(I,1) = VNEW(I,N+1)
         PNEW(I,N+1) = PNEW(I,1)
      END DO
-     UNEW(1,N+1) = UNEW(M+1,1)
-     VNEW(M+1,1) = VNEW(1,N+1)
-     PNEW(M+1,N+1) = PNEW(1,1)
+!$OMP END DO
 
      CALL timer_stop(idxt1)
 
+!$OMP SINGLE
      TIME = TIME + DT
+!$OMP END SINGLE
 
      !        TIME SMOOTHING AND UPDATE FOR NEXT CYCLE
      IF(NCYCLE .GT. 1) then
 
         CALL timer_start('Time smooth',idxt1)
 
-        DO J=1,N
-           DO I=1,M
+!$OMP DO SCHEDULE(static,chunk_size)
+        DO J=1,NP1
+           DO I=1,MP1
               UOLD(I,J) = U(I,J)+ALPHA*(UNEW(I,J)-2.*U(I,J)+UOLD(I,J))
+           END DO
+        END DO
+!$OMP END DO nowait
+!$OMP DO SCHEDULE(static,chunk_size)
+        DO J=1,NP1
+           DO I=1,MP1
               VOLD(I,J) = V(I,J)+ALPHA*(VNEW(I,J)-2.*V(I,J)+VOLD(I,J))
+           END DO
+        END DO
+!$OMP END DO nowait
+!$OMP DO SCHEDULE(static,chunk_size)
+        DO J=1,NP1
+           DO I=1,MP1
               POLD(I,J) = P(I,J)+ALPHA*(PNEW(I,J)-2.*P(I,J)+POLD(I,J))
+           END DO
+        END DO
+!$OMP END DO nowait
+!$OMP DO SCHEDULE(static,chunk_size)
+        DO J=1,NP1
+           DO I=1,MP1
               U(I,J) = UNEW(I,J)
+           END DO
+        END DO
+!$OMP END DO nowait
+!$OMP DO SCHEDULE(static,chunk_size)
+        DO J=1,NP1
+           DO I=1,MP1
               V(I,J) = VNEW(I,J)
+           END DO
+        END DO
+!$OMP END DO nowait
+!$OMP DO SCHEDULE(static,chunk_size)
+        DO J=1,NP1
+           DO I=1,MP1
               P(I,J) = PNEW(I,J)
            END DO
         END DO
+!$OMP END DO nowait
 
         CALL timer_stop(idxt1)
+!$OMP BARRIER
 
-        !           PERIODIC CONTINUATION
-        DO J=1,N
-           UOLD(M+1,J) = UOLD(1,J)
-           VOLD(M+1,J) = VOLD(1,J)
-           POLD(M+1,J) = POLD(1,J)
-           U(M+1,J) = U(1,J)
-           V(M+1,J) = V(1,J)
-           P(M+1,J) = P(1,J)
-        END DO
-        DO I=1,M
-           UOLD(I,N+1) = UOLD(I,1)
-           VOLD(I,N+1) = VOLD(I,1)
-           POLD(I,N+1) = POLD(I,1)
-           U(I,N+1) = U(I,1)
-           V(I,N+1) = V(I,1)
-           P(I,N+1) = P(I,1)
-        END DO
-        UOLD(M+1,N+1) = UOLD(1,1)
-        VOLD(M+1,N+1) = VOLD(1,1)
-        POLD(M+1,N+1) = POLD(1,1)
-        U(M+1,N+1) = U(1,1)
-        V(M+1,N+1) = V(1,1)
-        P(M+1,N+1) = P(1,1)
      else
 
         TDT = TDT+TDT
 
+!$OMP DO SCHEDULE(static,chunk_size)
         DO J=1,NP1
            DO I=1,MP1
               UOLD(I,J) = U(I,J)
@@ -346,7 +418,8 @@ PROGRAM shallow
 
      endif
 
-     IF( l_out .AND. (MOD(NCYCLE,MPRINT) .EQ. 0) ) then
+     IF( l_out .AND. (MOD(NCYCLE,MPRINT) .EQ. 0) .AND. (myid .EQ. 0)) then
+
 
         PTIME = TIME/3600.
         WRITE(6,350) NCYCLE,PTIME
@@ -370,6 +443,8 @@ PROGRAM shallow
      endif
 
   End do
+
+!$OMP END PARALLEL
 
   !  ** End of time loop ** 
   CALL timer_stop(idxt0)
