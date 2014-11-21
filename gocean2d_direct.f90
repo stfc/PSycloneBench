d118f4efa810c51913893d44cb9abad88f92a5c3
arp28 <arp28@b933007e-b0d1-11e3-9120-db60e37ba295>
2014-11-21 15:44:47 +0000

    WIP putting nemolite into single-invoke form

--- gocean2d_direct.f90
+++ gocean2d_direct.f90
@@ -35,7 +35,8 @@ program gocean2d
   ! Create the model grid. We use a NE offset (i.e. the U, V and F
   ! points immediately to the North and East of a T point all have the
   ! same i,j index).  This is the same offset scheme as used by NEMO.
-  model_grid = grid_type(ARAKAWA_C,                           &
+  model_grid = grid_type(ARAKAWA_C, &
+  !  BC_PERIODIC, BC_NON_PERIODIC ??
                          (/BC_EXTERNAL,BC_EXTERNAL,BC_NONE/), &
                          OFFSET_NE)
 
@@ -108,10 +109,7 @@ subroutine step(grid, istp, &
   use kind_params_mod
   use grid_mod
   use field_mod
-  use momentum_mod, only: invoke_momentum_u, invoke_momentum_v
-  use continuity_mod, only: invoke_continuity
-  use time_update_mod, only: invoke_next_sshu, invoke_next_sshv
-  use boundary_conditions_mod
+  use time_step_mod, only: invoke_time_step
   use gocean2d_io_mod, only: model_write
   implicit none
   type(grid_type), intent(in) :: grid
@@ -120,30 +118,10 @@ subroutine step(grid, istp, &
   type(r2d_field), intent(inout) :: ua, va, ssha, ssha_u, ssha_v
   type(r2d_field), intent(in) :: hu, hv, ht
 
-  CALL invoke_continuity(ssha, sshn, sshn_u, sshn_v, &
-                         hu, hv, un, vn)
+  call invoke_time_step(istp, ssha, ssha_u, ssha_v, &
+                        sshn, sshn_u, sshn_v, &
+                        hu, hv, ht, ua, va, un, vn)
 
-  CALL invoke_momentum_u(ua, un, vn, &
-                         hu, hv, ht, &
-                         ssha_u, sshn, sshn_u, sshn_v)
-
-  CALL invoke_momentum_v(va, un, vn, &
-                         hu, hv, ht, &
-                         ssha_v, sshn, sshn_u, sshn_v)
-
-  ! Apply open and solid boundary conditions
-  CALL invoke_bc_ssh(istp, ssha)
-  CALL invoke_bc_solid_u(ua)
-  CALL invoke_bc_solid_v(va)
-  CALL invoke_bc_flather_u(ua, hu, sshn_u)
-  CALL invoke_bc_flather_v(va, hv, sshn_v)
-
-  ! Time update of fields
-  call copy_field(ua, un)
-  call copy_field(va, vn)
-  call copy_field(ssha, sshn)
-  call invoke_next_sshu(sshn_u, sshn)
-  call invoke_next_sshv(sshn_v, sshn)
 
   call model_write(grid, istp, ht, sshn, un, vn)
 
