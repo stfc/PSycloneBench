8a22c64199768e60d16755b8ef939a99bc0b9bc8
arp28 <arp28@b933007e-b0d1-11e3-9120-db60e37ba295>
2015-01-02 15:15:37 +0000

    Inline both u- and v-momentum kernel code into time_step_mod

--- time_step_mod.f90
+++ time_step_mod.f90
@@ -10,11 +10,14 @@ contains
   subroutine invoke_time_step(istp, ssha, ssha_u, ssha_v, &
                               sshn_t, sshn_u, sshn_v, &
                               hu, hv, ht, ua, va, un, vn)
+    use kind_params_mod
     use timing_mod
     use field_mod
     use grid_mod
-    use model_mod,       only: rdt
-    use momentum_mod,    only: momentum_u_code, momentum_v_code
+    use model_mod,       only: rdt, cbfr, visc
+    use physical_params_mod, only: g, omega, d2r
+!    use momentum_mod,    only: momentum_v_code
+!    use momentum_mod,    only: momentum_u_code
     use continuity_mod,  only: continuity_code
     use time_update_mod, only: next_sshu_code, next_sshv_code
     use boundary_conditions_mod
@@ -26,6 +29,17 @@ contains
     ! Locals
     integer :: ji, jj
     integer :: M, N, idxt
+    ! Locals for momentum
+    REAL(wp) :: u_e, u_w, v_n, v_s
+    real(wp) :: v_nc, v_sc
+    real(wp) :: depe, depw, deps, depn
+    real(wp) :: hpg, adv, cor, vis
+    real(wp) :: dudx_e, dudx_w, dudy_s, dudy_n
+    real(wp) :: uu_e, uu_n, uu_s, uu_w
+    real(wp) :: u_ec, u_wc, vv_e, vv_n, vv_s, vv_w
+    real(wp) :: dvdx_e, dvdx_w, dvdy_n, dvdy_s
+
+    ! end locals for momentum-u
 
     M  = ssha%grid%simulation_domain%xstop
     N  = ssha%grid%simulation_domain%ystop
@@ -84,19 +98,107 @@ contains
 ! SIMD
       do ji = 2, M-1, 1
 
-        call momentum_u_code(ji, jj, &
-                             ua%data, un%data, vn%data, &
-                             hu%data, hv%data, ht%data, &
-                             ssha_u%data, sshn_t%data,  &
-                             sshn_u%data, sshn_v%data,  &
-                             un%grid%tmask,  &
-                             un%grid%dx_u,   &
-                             un%grid%dx_v,   &
-                             un%grid%dx_t,   &
-                             un%grid%dy_u,   &
-                             un%grid%dy_t,   &
-                             un%grid%area_u, &
-                             un%grid%gphiu)
+!!$        call momentum_u_code(ji, jj, &
+!!$                             ua%data, un%data, vn%data, &
+!!$                             hu%data, hv%data, ht%data, &
+!!$                             ssha_u%data, sshn_t%data,  &
+!!$                             sshn_u%data, sshn_v%data,  &
+!!$                             un%grid%tmask,  &
+!!$                             un%grid%dx_u,   &
+!!$                             un%grid%dx_v,   &
+!!$                             un%grid%dx_t,   &
+!!$                             un%grid%dy_u,   &
+!!$                             un%grid%dy_t,   &
+!!$                             un%grid%area_u, &
+!!$                             un%grid%gphiu)
+
+    IF(un%grid%tmask(ji,jj) + un%grid%tmask(ji+1,jj) <= 0)  CYCLE   !jump over non-computational domain
+    IF(un%grid%tmask(ji,jj) <= 0 .OR. un%grid%tmask(ji+1,jj) <= 0)  CYCLE !jump over boundary u
+
+    u_e  = 0.5 * (un%data(ji,jj) + un%data(ji+1,jj)) * un%grid%dy_t(ji+1,jj)   !add length scale.
+    depe = ht%data(ji+1,jj) + sshn_t%data(ji+1,jj)
+
+    u_w  = 0.5 * (un%data(ji,jj) + un%data(ji-1,jj)) * un%grid%dy_t(ji,jj)     !add length scale
+    depw = ht%data(ji,jj) + sshn_t%data(ji,jj)
+
+    v_sc = 0.5_wp * (vn%data(ji,jj-1) + vn%data(ji+1,jj-1))
+    v_s  = 0.5_wp * v_sc * (un%grid%dx_v(ji,jj-1) + un%grid%dx_v(ji+1,jj-1))
+    deps = 0.5_wp * (hv%data(ji,jj-1) + sshn_v%data(ji,jj-1) + hv%data(ji+1,jj-1) + &
+                     sshn_v%data(ji+1,jj-1))
+
+    v_nc = 0.5_wp * (vn%data(ji,jj) + vn%data(ji+1,jj))
+    v_n  = 0.5_wp * v_nc * (un%grid%dx_v(ji,jj) + un%grid%dx_v(ji+1,jj))
+    depn = 0.5_wp * (hv%data(ji,jj) + sshn_v%data(ji,jj) + hv%data(ji+1,jj) + &
+                     sshn_v%data(ji+1,jj))
+
+    ! -advection (currently first order upwind)
+    uu_w = (0.5_wp - SIGN(0.5_wp, u_w)) * un%data(ji,jj)              + & 
+         & (0.5_wp + SIGN(0.5_wp, u_w)) * un%data(ji-1,jj) 
+    uu_e = (0.5_wp + SIGN(0.5_wp, u_e)) * un%data(ji,jj)              + & 
+         & (0.5_wp - SIGN(0.5_wp, u_e)) * un%data(ji+1,jj) 
+
+    IF(un%grid%tmask(ji,jj-1) <=0 .OR. un%grid%tmask(ji+1,jj-1) <= 0) THEN   
+       uu_s = (0.5_wp - SIGN(0.5_wp, v_s)) * un%data(ji,jj)   
+    ELSE
+       uu_s = (0.5_wp - SIGN(0.5_wp, v_s)) * un%data(ji,jj)              + & 
+            & (0.5_wp + SIGN(0.5_wp, v_s)) * un%data(ji,jj-1) 
+    END If
+
+    IF(un%grid%tmask(ji,jj+1) <=0 .OR. un%grid%tmask(ji+1,jj+1) <= 0) THEN   
+       uu_n = (0.5_wp + SIGN(0.5_wp, v_n)) * un%data(ji,jj)
+    ELSE
+       uu_n = (0.5_wp + SIGN(0.5_wp, v_n)) * un%data(ji,jj)              + & 
+            & (0.5_wp - SIGN(0.5_wp, v_n)) * un%data(ji,jj+1)
+    END IF
+
+    adv = uu_w * u_w * depw - uu_e * u_e * depe + &
+          uu_s * v_s * deps - uu_n * v_n * depn
+    !end kernel u adv 
+
+    ! -viscosity
+
+    !kernel  u vis 
+    dudx_e = (un%data(ji+1,jj) - un%data(ji,  jj)) / un%grid%dx_t(ji+1,jj) * &
+             (ht%data(ji+1,jj) + sshn_t%data(ji+1,jj))
+    dudx_w = (un%data(ji,  jj) - un%data(ji-1,jj)) / un%grid%dx_t(ji,  jj) * &
+             (ht%data(ji,  jj) + sshn_t%data(ji,  jj))
+    IF(un%grid%tmask(ji,jj-1) <=0 .OR. un%grid%tmask(ji+1,jj-1) <= 0) THEN   
+       dudy_s = 0.0_wp !slip boundary
+    ELSE
+       dudy_s = (un%data(ji,jj) - un%data(ji,jj-1)) / (un%grid%dy_u(ji,jj) + un%grid%dy_u(ji,jj-1)) * &
+            & (hu%data(ji,jj) + sshn_u%data(ji,jj) + hu%data(ji,jj-1) + sshn_u%data(ji,jj-1))
+    END IF
+
+    IF(un%grid%tmask(ji,jj+1) <= 0 .OR. un%grid%tmask(ji+1,jj+1) <= 0) THEN   
+       dudy_n = 0.0_wp ! slip boundary
+    ELSE
+       dudy_n = (un%data(ji,jj+1) - un%data(ji,jj)) / (un%grid%dy_u(ji,jj) + un%grid%dy_u(ji,jj+1)) * &
+            & (hu%data(ji,jj) + sshn_u%data(ji,jj) + hu%data(ji,jj+1) + sshn_u%data(ji,jj+1))
+    END If
+
+    vis = (dudx_e - dudx_w ) * un%grid%dy_u(ji,jj)  + &
+         & (dudy_n - dudy_s ) * un%grid%dx_u(ji,jj) * 0.5_wp  
+    vis = visc * vis   !visc will be an array visc(1:jpijglou) 
+    !for variable viscosity, such as turbulent viscosity
+    !End  kernel u vis 
+
+    ! -Coriolis' force (can be implemented implicitly)
+    !kernel cor 
+    cor = 0.5_wp * (2._wp * omega * SIN(un%grid%gphiu(ji,jj) * d2r) * (v_sc + v_nc)) * &
+         & un%grid%area_u(ji,jj) * (hu%data(ji,jj) + sshn_u%data(ji,jj))
+    !end kernel cor 
+
+    ! -pressure gradient
+    !start kernel hpg 
+    hpg = -g * (hu%data(ji,jj) + sshn_u%data(ji,jj)) * un%grid%dy_u(ji,jj) * &
+           (sshn_t%data(ji+1,jj) - sshn_t%data(ji,jj))
+    !end kernel hpg 
+    ! -linear bottom friction (implemented implicitly.
+    !kernel ua calculation 
+    ua%data(ji,jj) = (un%data(ji,jj) * (hu%data(ji,jj) + sshn_u%data(ji,jj)) + rdt * &
+                 (adv + vis + cor + hpg) / un%grid%area_u(ji,jj)) / &
+                (hu%data(ji,jj) + ssha_u%data(ji,jj)) / (1.0_wp + cbfr * rdt) 
+
       end do
     end do
  
@@ -105,19 +207,113 @@ contains
 ! SIMD
       do ji = 2, M, 1
 
-        call momentum_v_code(ji, jj, &
-                             va%data, un%data, vn%data, &
-                             hu%data, hv%data, ht%data, &
-                             ssha_v%data, sshn_t%data,  &
-                             sshn_u%data, sshn_v%data,  &
-                             vn%grid%tmask,    &
-                             vn%grid%dx_v,     &
-                             vn%grid%dx_t,     &
-                             vn%grid%dy_u,     &
-                             vn%grid%dy_v,     &
-                             vn%grid%dy_t,     &
-                             vn%grid%area_v,   &
-                             vn%grid%gphiv)
+!!$        call momentum_v_code(ji, jj, &
+!!$                             va%data, un%data, vn%data, &
+!!$                             hu%data, hv%data, ht%data, &
+!!$                             ssha_v%data, sshn_t%data,  &
+!!$                             sshn_u%data, sshn_v%data,  &
+!!$                             vn%grid%tmask,    &
+!!$                             vn%grid%dx_v,     &
+!!$                             vn%grid%dx_t,     &
+!!$                             vn%grid%dy_u,     &
+!!$                             vn%grid%dy_v,     &
+!!$                             vn%grid%dy_t,     &
+!!$                             vn%grid%area_v,   &
+!!$                             vn%grid%gphiv)
+
+    IF(vn%grid%tmask(ji,jj) + vn%grid%tmask(ji+1,jj) <= 0)  cycle !jump over non-computatinal domain
+    IF(vn%grid%tmask(ji,jj) <= 0 .OR. vn%grid%tmask(ji,jj+1) <= 0) cycle !jump over v boundary cells
+
+    ! kernel v adv 
+    v_n  = 0.5 * (vn%data(ji,jj) + vn%data(ji,jj+1)) * vn%grid%dx_t(ji,jj+1)  !add length scale.
+    depn = ht%data(ji,jj+1) + sshn_t%data(ji,jj+1)
+
+    v_s  = 0.5 * (vn%data(ji,jj) + vn%data(ji,jj-1)) * vn%grid%dx_t(ji,jj)    !add length scale
+    deps = ht%data(ji,jj) + sshn_t%data(ji,jj)
+
+    u_wc = 0.5_wp * (un%data(ji-1,jj) + un%data(ji-1,jj+1))
+    u_w  = 0.5_wp * u_wc * (vn%grid%dy_u(ji-1,jj) + vn%grid%dy_u(ji-1,jj+1))
+    depw = 0.50_wp * (hu%data(ji-1,jj) + sshn_u%data(ji-1,jj) + &
+                      hu%data(ji-1,jj+1) + sshn_u%data(ji-1,jj+1))
+
+    u_ec = 0.5_wp * (un%data(ji,jj) + un%data(ji,jj+1))
+    u_e  = 0.5_wp * u_ec * (vn%grid%dy_u(ji,jj) + vn%grid%dy_u(ji,jj+1))
+    depe = 0.50_wp * (hu%data(ji,jj) + sshn_u%data(ji,jj) + &
+                      hu%data(ji,jj+1) + sshn_u%data(ji,jj+1))
+
+    ! -advection (currently first order upwind)
+    vv_s = (0.5_wp - SIGN(0.5_wp, v_s)) * vn%data(ji,jj)     + & 
+         & (0.5_wp + SIGN(0.5_wp, v_s)) * vn%data(ji,jj-1) 
+    vv_n = (0.5_wp + SIGN(0.5_wp, v_n)) * vn%data(ji,jj)     + & 
+         & (0.5_wp - SIGN(0.5_wp, v_n)) * vn%data(ji,jj+1) 
+
+    IF(vn%grid%tmask(ji-1,jj) <= 0 .OR. vn%grid%tmask(ji-1,jj+1) <= 0) THEN   
+       vv_w = (0.5_wp - SIGN(0.5_wp, u_w)) * vn%data(ji,jj)  
+    ELSE
+       vv_w = (0.5_wp - SIGN(0.5_wp, u_w)) * vn%data(ji,jj)    + & 
+            & (0.5_wp + SIGN(0.5_wp, u_w)) * vn%data(ji-1,jj) 
+    END If
+
+    IF(vn%grid%tmask(ji+1,jj) <= 0 .OR. vn%grid%tmask(ji+1,jj+1) <= 0) THEN
+       vv_e = (0.5_wp + SIGN(0.5_wp, u_e)) * vn%data(ji,jj)
+    ELSE
+       vv_e = (0.5_wp + SIGN(0.5_wp, u_e)) * vn%data(ji,jj)  + & 
+              (0.5_wp - SIGN(0.5_wp, u_e)) * vn%data(ji+1,jj)
+    END IF
+
+    adv = vv_w * u_w * depw - vv_e * u_e * depe + &
+          vv_s * v_s * deps - vv_n * v_n * depn
+
+    !end kernel v adv 
+
+    ! -viscosity
+
+    
+    !kernel v dis 
+    dvdy_n = (vn%data(ji,jj+1) - vn%data(ji,  jj)) / vn%grid%dy_t(ji,jj+1) * &
+                          (ht%data(ji,jj+1) + sshn_t%data(ji,jj+1))
+    dvdy_s = (vn%data(ji,  jj) - vn%data(ji,jj-1)) / vn%grid%dy_t(ji,  jj) * &
+                          (ht%data(ji,  jj) + sshn_t%data(ji,  jj))
+
+    IF(vn%grid%tmask(ji-1,jj) <= 0 .OR. vn%grid%tmask(ji-1,jj+1) <= 0) THEN
+       dvdx_w = 0.0_wp !slip boundary
+    ELSE
+       dvdx_w = (vn%data(ji,jj) - vn%data(ji-1,jj)) / &
+                (vn%grid%dx_v(ji,jj) + vn%grid%dx_v(ji-1,jj)) * &
+                (hv%data(ji,jj) + sshn_v%data(ji,jj) + hv%data(ji-1,jj) + sshn_v%data(ji-1,jj))
+    END IF
+
+    IF(vn%grid%tmask(ji+1,jj) <= 0 .OR. vn%grid%tmask(ji+1,jj+1) <= 0) THEN
+       dvdx_e = 0.0_wp ! slip boundary
+    ELSE
+       dvdx_e = (vn%data(ji+1,jj) - vn%data(ji,jj)) / (vn%grid%dx_v(ji,jj) + vn%grid%dx_v(ji+1,jj)) * &
+                  (hv%data(ji,jj) + sshn_v%data(ji,jj) + hv%data(ji+1,jj) + sshn_v%data(ji+1,jj))
+    END If
+
+    vis = (dvdy_n - dvdy_s ) * vn%grid%dx_v(ji,jj)  + &
+          (dvdx_e - dvdx_w ) * vn%grid%dy_v(ji,jj) * 0.5_wp  
+
+    vis = visc * vis   !visc will be a array visc(1:jpijglou) 
+    !for variable viscosity, such as turbulent viscosity
+    !end kernel v dis 
+
+    ! -Coriolis' force (can be implemented implicitly)
+    !kernel v cor 
+    cor = -0.5_wp*(2._wp * omega * SIN(vn%grid%gphiv(ji,jj) * d2r) * (u_ec + u_wc)) * &
+               vn%grid%area_v(ji,jj) * (hv%data(ji,jj) + sshn_v%data(ji,jj))
+    !end kernel v cor 
+
+    ! -pressure gradient
+    !kernel v hpg 
+    hpg = -g * (hv%data(ji,jj) + sshn_v%data(ji,jj)) * vn%grid%dx_v(ji,jj) * &
+           (sshn_t%data(ji,jj+1) - sshn_t%data(ji,jj))
+    !kernel v hpg 
+
+    ! -linear bottom friction (implemented implicitly.
+    !kernel ua calculation 
+    va%data(ji,jj) = (vn%data(ji,jj) * (hv%data(ji,jj) + sshn_v%data(ji,jj)) + &
+                 rdt * (adv + vis + cor + hpg) / vn%grid%area_v(ji,jj) ) / &
+                 ((hv%data(ji,jj) + ssha_v%data(ji,jj))) / (1.0_wp + cbfr * rdt) 
 
       end do
     end do
