0ebf3ecea481d7ca259cd0e0329e47c3f0fb5607
arp28 <arp28@b933007e-b0d1-11e3-9120-db60e37ba295>
2015-07-08 10:04:39 +0000

    Revert to orig look-up of loop bounds in invoke_time_step()

--- nemolite2d_psykal/time_step_mod.f90
+++ nemolite2d_psykal/time_step_mod.f90
@@ -24,43 +24,9 @@ contains
     type(r2d_field), intent(in)    :: hu, hv, ht
     ! Locals
     integer :: ji, jj
-    integer :: M, N
 
-    M  = ssha%grid%simulation_domain%xstop
-    N  = ssha%grid%simulation_domain%ystop
-
-    ! In the general case we have to reason about whether or not the
-    ! domain has PBCs and what sort of offset convention the kernels
-    ! use. However, this is a middle layer specific to NEMOLite2D and
-    ! therefore we know that we have no periodic BCs and are using a
-    ! NE stagger
-    !txstart = 2 ! grid%simulation_domain%xstart
-    !tystart = 2 ! grid%simulation_domain%ystart
-
-    !uxstart = 2 ! grid%simulation_domain%xstart
-    !uxstop  = M - 1
-    !uystart = 2 ! grid%simulation_domain%ystart
-    !uystop  = N
-
-    !vxstart = 2 ! grid%simulation_domain%xstart
-    !vxstop  = M
-    !vystart = 2 ! grid%simulation_domain%ystart
-    !vystop  = N - 1
-
-    !uwhole_xstart = 1 ! uxstart - NBOUNDARY
-    !uwhole_xstop  = M ! uxstop  + NBOUNDARY
-    !uwhole_ystart = 1 ! uystart - NBOUNDARY
-    !uwhole_ystop  = N+1 ! uystop  + NBOUNDARY
-
-    !vwhole_xstart = 1 ! vxstart - NBOUNDARY
-    !vwhole_xstop  = M+1 ! vxstop  + NBOUNDARY
-    !vwhole_ystart = 1 ! vystart - NBOUNDARY
-    !vwhole_ystop  = N ! vystop  + NBOUNDARY
-
-!    do jj = ssha%internal%ystart, ssha%internal%ystop, 1
-!      do ji = ssha%internal%xstart, ssha%internal%xstop, 1
-    do jj = 2, N, 1
-      do ji = 2, M, 1
+    do jj = ssha%internal%ystart, ssha%internal%ystop, 1
+      do ji = ssha%internal%xstart, ssha%internal%xstop, 1
 
         call continuity_code(ji, jj,                             &
                              ssha%data, sshn_t%data,             &
@@ -70,10 +36,8 @@ contains
       end do
     end do
 
-!    do jj = ua%internal%ystart, ua%internal%ystop, 1
-!      do ji = ua%internal%xstart, ua%internal%xstop, 1
-    do jj = 2, N, 1
-      do ji = 2, M-1, 1
+    do jj = ua%internal%ystart, ua%internal%ystop, 1
+      do ji = ua%internal%xstart, ua%internal%xstop, 1
 
         call momentum_u_code(ji, jj, &
                              ua%data, un%data, vn%data, &
@@ -91,8 +55,8 @@ contains
       end do
     end do
  
-    do jj = 2, N-1, 1
-      do ji = 2, M, 1
+    do jj = va%internal%ystart, va%internal%ystop, 1
+      do ji = va%internal%xstart, va%internal%xstop, 1
 
         call momentum_v_code(ji, jj, &
                              va%data, un%data, vn%data, &
@@ -110,50 +74,37 @@ contains
 
     ! Apply open and solid boundary conditions
 
-!    DO jj = ssha%internal%ystart, ssha%internal%ystop 
-!       DO ji = ssha%internal%xstart, ssha%internal%xstop 
-    DO jj = 2, N
-       DO ji = 2, M
+    DO jj = ssha%internal%ystart, ssha%internal%ystop 
+       DO ji = ssha%internal%xstart, ssha%internal%xstop 
+
           call bc_ssh_code(ji, jj, &
                            istp, ssha%data, ssha%grid%tmask)
        END DO
     END DO
 
 
-!    do jj = uwhole_ystart, uwhole_ystop, 1
-!       do ji = uwhole_xstart, uwhole_xstop, 1
-    do jj = 1, N+1, 1
-       do ji = 1, M, 1
+    do jj = ua%whole%ystart, ua%whole%ystop, 1
+       do ji = ua%whole%xstart, ua%whole%xstop, 1
           call bc_solid_u_code(ji, jj, ua%data, ua%grid%tmask)
        end do
     end do
 
-!    DO jj = va%whole%ystart, va%whole%ystop, 1 
-!       DO ji = va%whole%xstart, va%whole%xstop, 1
-!    do jj = vwhole_ystart, vwhole_ystop, 1
-!       do ji = vwhole_xstart, vwhole_xstop, 1
-    do jj = 1, N, 1
-       do ji = 1, M+1, 1
+    DO jj = va%whole%ystart, va%whole%ystop, 1 
+       DO ji = va%whole%xstart, va%whole%xstop, 1
           call bc_solid_v_code(ji,jj,va%data,va%grid%tmask)
       end do
     end do
-
-!    DO jj = uwhole_ystart, uwhole_ystop, 1
-!       DO ji = uwhole_xstart, uwhole_xstop, 1
-    DO jj = 1, N+1, 1
-       DO ji = 1, M, 1
+ 
+    DO jj = ua%whole%ystart, ua%whole%ystop, 1 
+       DO ji = ua%whole%xstart, ua%whole%xstop, 1
           call bc_flather_u_code(ji,jj, &
                                  ua%data, hu%data, sshn_u%data, &
                                  ua%grid%tmask)
        END DO
     END DO
 
-!    DO jj = va%whole%ystart, va%whole%ystop, 1 
-!       DO ji = va%whole%xstart, va%whole%xstop, 1
-!     DO jj = vwhole_ystart, vwhole_ystop, 1
-!       DO ji = vwhole_xstart, vwhole_xstop, 1
-     DO jj = 1, N, 1
-       DO ji = 1, M+1, 1
+    DO jj = va%whole%ystart, va%whole%ystop, 1 
+       DO ji = va%whole%xstart, va%whole%xstop, 1
           call bc_flather_v_code(ji,jj, &
                                  va%data, hv%data, sshn_v%data, &
                                  va%grid%tmask)
@@ -165,8 +116,8 @@ contains
     call copy_field(va, vn)
     call copy_field(ssha, sshn_t)
 
-    do jj = 2, N, 1
-      do ji = 2, M-1, 1
+    DO jj = sshn_u%internal%ystart, sshn_u%internal%ystop 
+       DO ji = sshn_u%internal%xstart, sshn_u%internal%xstop 
 
          call next_sshu_code(ji, jj, sshn_u%data, sshn_t%data, &
                             sshn_u%grid%tmask,                 &
@@ -174,8 +125,8 @@ contains
       end do
     end do
 
-    do jj = 2, N-1, 1
-      do ji = 2, M, 1
+    DO jj = sshn_v%internal%ystart, sshn_v%internal%ystop 
+       DO ji = sshn_v%internal%xstart, sshn_v%internal%xstop 
 
         call next_sshv_code(ji, jj,                   &
                             sshn_v%data, sshn_t%data, &
