f9342b457f37bb46827f6fc5cab2d1294b5d8dff
arp28 <arp28@b933007e-b0d1-11e3-9120-db60e37ba295>
2015-09-14 09:30:02 +0000

    #2: add fusion of field-copy loops to code-gen script

--- nemolite2d_psykal/nemolite2d_omp_transform.py
+++ nemolite2d_psykal/nemolite2d_omp_transform.py
@@ -12,16 +12,30 @@ from psyGen import TransInfo
 t=TransInfo()
 ltrans = t.get_trans_name('GOceanOMPLoopTrans')
 rtrans = t.get_trans_name('OMPParallelTrans')
+ftrans = t.get_trans_name('GOceanLoopFuse')
 
 schedule=psy.invokes.get('invoke_0').schedule
 #schedule.view()
 new_schedule=schedule
+
+# Fuse the three field-copy operations
+# First the outer loops...
+new_schedule, memento = ftrans.apply(schedule.children[8],
+                                     schedule.children[9])
+new_schedule, memento = ftrans.apply(new_schedule.children[8],
+                                     new_schedule.children[9])
+# Then the inner loops...
+new_schedule, memento = ftrans.apply(schedule.children[8].children[0],
+                                     schedule.children[8].children[1])
+new_schedule, memento = ftrans.apply(schedule.children[8].children[0],
+                                     schedule.children[8].children[1])
 
 # Apply the OpenMP Loop transformation to *every* loop 
 # in the schedule
 for child in schedule.children:
     newschedule,memento=ltrans.apply(child)
     schedule = newschedule
+
 
 # Enclose all of these loops within a single OpenMP
 # PARALLEL region
