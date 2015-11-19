d2f7d00d00ccafc22572b12db7fadf2c7bb41d9c
arp28 <arp28@b933007e-b0d1-11e3-9120-db60e37ba295>
2015-11-19 10:12:04 +0000

    Convert nemolite2d-omp script into form that can be passed to generate using -s flag. Change Makefile to do so.

--- nemolite2d_psykal/nemolite2d_omp_transform.py
+++ nemolite2d_psykal/nemolite2d_omp_transform.py
@@ -1,31 +1,25 @@
-# Python script which uses PSyclone to apply transformations
-# before generating the PSy layer Fortran code.
-from parse import parse,ParseError
-from psyGen import PSyFactory,GenerationError
-api="gocean1.0"
-filename="nemolite2d_alg.f90"
-ast,invokeInfo=parse(filename,api=api,invoke_name="invoke")
-psy=PSyFactory(api).create(invokeInfo)
-#print psy.invokes.names
+def trans(psy):
+    ''' Python script intended to be passed to PSyclone's generate()
+    function via the -s option. Applies OpenMP to every loop before
+    enclosing them all within a single OpenMP PARALLEL region. '''
 
-from psyGen import TransInfo
-t=TransInfo()
-ltrans = t.get_trans_name('GOceanOMPLoopTrans')
-rtrans = t.get_trans_name('OMPParallelTrans')
+    from psyGen import TransInfo
+    tinfo = TransInfo()
+    ltrans = tinfo.get_trans_name('GOceanOMPLoopTrans')
+    rtrans = tinfo.get_trans_name('OMPParallelTrans')
 
-schedule=psy.invokes.get('invoke_0').schedule
-#schedule.view()
-new_schedule=schedule
+    schedule = psy.invokes.get('invoke_0').schedule
+    # schedule.view()
 
-# Apply the OpenMP Loop transformation to *every* loop 
-# in the schedule
-for child in schedule.children:
-    newschedule,memento=ltrans.apply(child)
-    schedule = newschedule
+    # Apply the OpenMP Loop transformation to *every* loop
+    # in the schedule
+    for child in schedule.children:
+        newschedule, _ = ltrans.apply(child)
+        schedule = newschedule
 
-# Enclose all of these loops within a single OpenMP
-# PARALLEL region
-newschedule,memento = rtrans.apply(schedule.children)
+    # Enclose all of these loops within a single OpenMP
+    # PARALLEL region
+    newschedule, _ = rtrans.apply(schedule.children)
 
-psy.invokes.get('invoke_0')._schedule=schedule
-print psy.gen
+    psy.invokes.get('invoke_0').schedule = newschedule
+    return psy
