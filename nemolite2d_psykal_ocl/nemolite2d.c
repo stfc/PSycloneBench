c6b89156c2e3882811a7ae0113bdc0cdead16872
arp28 <arp28@b933007e-b0d1-11e3-9120-db60e37ba295>
2017-03-30 14:15:46 +0000

    #4 Tidy-up top-level driver code

--- nemolite2d_psykal_ocl/nemolite2d.c
+++ nemolite2d_psykal_ocl/nemolite2d.c
@@ -5,17 +5,19 @@
 int main(){
   int nx = 128;
   int ny = 128;
-  int ji, jj;
+  int ji, jj, idx;
   double *ssha, *sshn, *sshn_u, *sshn_v;
   double *hu, *hv, *un, *vn;
   double rdt;
   double *e12t;
   double dep_const = 2.0;
-  
+  double dx = 0.5, dy = 0.5;
   ji = 1;
   jj = 1;
   rdt = 0.5;
 
+  /* Field initialisation */
+  
   ssha = malloc(nx*ny*sizeof(double));
   sshn = malloc(nx*ny*sizeof(double));
   sshn_u = malloc(nx*ny*sizeof(double));
@@ -26,23 +28,31 @@ int main(){
   vn = malloc(nx*ny*sizeof(double));
   e12t = malloc(nx*ny*sizeof(double));
 
-  double *huptr = &(hu[0]);
-  double *hvptr = &(hv[0]);
   for(jj=0;jj<ny;jj++){
     for(ji=0;ji<nx;ji++){
-      *(huptr++) = dep_const;
-      *(hvptr++) = dep_const;
+      idx = jj*nx + ji;
+      hu[idx] = dep_const;
+      hv[idx] = dep_const;
+      un[idx] = ji;
+      vn[idx] = jj;
+      sshn_u[idx] = 0.0;
+      sshn_v[idx] = 0.0;
+      sshn[idx] = 1.0;
+      e12t[idx] = dx*dy;
+      ssha[idx] = 0.0;
     }
   }
-  
-  for(jj=0;jj<ny;jj++){
-    for(ji=0;ji<nx;ji++){
+
+  /* Run the kernel */
+  for(jj=1;jj<ny;jj++){
+    for(ji=1;ji<nx;ji++){
       continuity_code(ji,jj, nx,                     
 		      ssha, sshn, sshn_u, sshn_v, hu, hv,
 		      un, vn, rdt, e12t);
     }
   }
 
+  /* Compute and output a checksum */
   double *sshaptr = &(ssha[0]);
   double sum = 0.0;
   for(jj=0;jj<ny;jj++){
