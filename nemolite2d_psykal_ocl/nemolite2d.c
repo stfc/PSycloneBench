cae4bde89d8628917bd6aa6a91ef5994a605b4e8
arp28 <arp28@b933007e-b0d1-11e3-9120-db60e37ba295>
2017-04-13 15:23:44 +0000

    #4 Break-out kernel construction and other utils into separate source file

--- nemolite2d_psykal_ocl/nemolite2d.c
+++ nemolite2d_psykal_ocl/nemolite2d.c
@@ -4,6 +4,7 @@
 
 /* Header for the C version of the kernel */
 #include "continuity.h"
+#include "opencl_utils.h"
 
 #ifdef __APPLE__
 #include <OpenCL/opencl.h>
@@ -11,124 +12,8 @@
 #include <CL/cl.h>
 #endif
 
-#define MAX_SOURCE_SIZE (0x100000)
 /** Maximum number of OpenCL devices we will query */
 #define MAX_DEVICES 4
-#define VERBOSE 1
-
-const char* OCL_GetErrorString(cl_int error)
-{
-    switch (error)
-    {
-    case CL_SUCCESS:
-        return "CL_SUCCESS";
-    case CL_DEVICE_NOT_FOUND:
-        return "CL_DEVICE_NOT_FOUND";
-    case CL_DEVICE_NOT_AVAILABLE:
-        return "CL_DEVICE_NOT_AVAILABLE";
-    case CL_COMPILER_NOT_AVAILABLE:
-        return "CL_COMPILER_NOT_AVAILABLE";
-    case CL_MEM_OBJECT_ALLOCATION_FAILURE:
-        return "CL_MEM_OBJECT_ALLOCATION_FAILURE";
-    case CL_OUT_OF_RESOURCES:
-        return "CL_OUT_OF_RESOURCES";
-    case CL_OUT_OF_HOST_MEMORY:
-        return "CL_OUT_OF_HOST_MEMORY";
-    case CL_PROFILING_INFO_NOT_AVAILABLE:
-        return "CL_PROFILING_INFO_NOT_AVAILABLE";
-    case CL_MEM_COPY_OVERLAP:
-        return "CL_MEM_COPY_OVERLAP";
-    case CL_IMAGE_FORMAT_MISMATCH:
-        return "CL_IMAGE_FORMAT_MISMATCH";
-    case CL_IMAGE_FORMAT_NOT_SUPPORTED:
-        return "CL_IMAGE_FORMAT_NOT_SUPPORTED";
-    case CL_BUILD_PROGRAM_FAILURE:
-        return "CL_BUILD_PROGRAM_FAILURE";
-    case CL_MAP_FAILURE:
-        return "CL_MAP_FAILURE";
-    case CL_INVALID_VALUE:
-        return "CL_INVALID_VALUE";
-    case CL_INVALID_DEVICE_TYPE:
-        return "CL_INVALID_DEVICE_TYPE";
-    case CL_INVALID_PLATFORM:
-        return "CL_INVALID_PLATFORM";
-    case CL_INVALID_DEVICE:
-        return "CL_INVALID_DEVICE";
-    case CL_INVALID_CONTEXT:
-        return "CL_INVALID_CONTEXT";
-    case CL_INVALID_QUEUE_PROPERTIES:
-        return "CL_INVALID_QUEUE_PROPERTIES";
-    case CL_INVALID_COMMAND_QUEUE:
-        return "CL_INVALID_COMMAND_QUEUE";
-    case CL_INVALID_HOST_PTR:
-        return "CL_INVALID_HOST_PTR";
-    case CL_INVALID_MEM_OBJECT:
-        return "CL_INVALID_MEM_OBJECT";
-    case CL_INVALID_IMAGE_FORMAT_DESCRIPTOR:
-        return "CL_INVALID_IMAGE_FORMAT_DESCRIPTOR";
-    case CL_INVALID_IMAGE_SIZE:
-        return "CL_INVALID_IMAGE_SIZE";
-    case CL_INVALID_SAMPLER:
-        return "CL_INVALID_SAMPLER";
-    case CL_INVALID_BINARY:
-        return "CL_INVALID_BINARY";
-    case CL_INVALID_BUILD_OPTIONS:
-        return "CL_INVALID_BUILD_OPTIONS";
-    case CL_INVALID_PROGRAM:
-        return "CL_INVALID_PROGRAM";
-    case CL_INVALID_PROGRAM_EXECUTABLE:
-        return "CL_INVALID_PROGRAM_EXECUTABLE";
-    case CL_INVALID_KERNEL_NAME:
-        return "CL_INVALID_KERNEL_NAME";
-    case CL_INVALID_KERNEL_DEFINITION:
-        return "CL_INVALID_KERNEL_DEFINITION";
-    case CL_INVALID_KERNEL:
-        return "CL_INVALID_KERNEL";
-    case CL_INVALID_ARG_INDEX:
-        return "CL_INVALID_ARG_INDEX";
-    case CL_INVALID_ARG_VALUE:
-        return "CL_INVALID_ARG_VALUE";
-    case CL_INVALID_ARG_SIZE:
-        return "CL_INVALID_ARG_SIZE";
-    case CL_INVALID_KERNEL_ARGS:
-        return "CL_INVALID_KERNEL_ARGS";
-    case CL_INVALID_WORK_DIMENSION:
-        return "CL_INVALID_WORK_DIMENSION";
-    case CL_INVALID_WORK_GROUP_SIZE:
-        return "CL_INVALID_WORK_GROUP_SIZE";
-    case CL_INVALID_WORK_ITEM_SIZE:
-        return "CL_INVALID_WORK_ITEM_SIZE";
-    case CL_INVALID_GLOBAL_OFFSET:
-        return "CL_INVALID_GLOBAL_OFFSET";
-    case CL_INVALID_EVENT_WAIT_LIST:
-        return "CL_INVALID_EVENT_WAIT_LIST";
-    case CL_INVALID_EVENT:
-        return "CL_INVALID_EVENT";
-    case CL_INVALID_OPERATION:
-        return "CL_INVALID_OPERATION";
-    case CL_INVALID_GL_OBJECT:
-        return "CL_INVALID_GL_OBJECT";
-    case CL_INVALID_BUFFER_SIZE:
-        return "CL_INVALID_BUFFER_SIZE";
-    case CL_INVALID_MIP_LEVEL:
-        return "CL_INVALID_MIP_LEVEL";
-    case CL_INVALID_GLOBAL_WORK_SIZE:
-        return "CL_INVALID_GLOBAL_WORK_SIZE";
-        // unknown
-    default:
-        return "unknown error code";
-    }
-}
-
-void check_status(char *text, cl_int err){
-  if(err != CL_SUCCESS){
-    fprintf(stderr, "Hit error: %s: %s\n", text, OCL_GetErrorString(err));
-    exit(1);
-  }
-  if(VERBOSE){
-    fprintf(stdout, "Called %s OK\n", text); 
-  }
-}
 
 /** Compute a checksum for a double precision array of nx*ny values */
 double checksum(double *array, int nx, int ny, int xstart, int ystart){
@@ -185,20 +70,6 @@ int main(){
 
   /*------------------------------------------------------------*/
   /* OpenCL initialisation */
-  FILE *fp;
-  char fileName[] = "./continuity_kern.c";
-  char *source_str;
-  size_t source_size;
-
-  /* Load the source code containing the kernel*/
-  fp = fopen(fileName, "r");
-  if (!fp) {
-    fprintf(stderr, "Failed to load kernel %s.\n", fileName);
-    exit(1);
-  }
-  source_str = (char*)malloc(MAX_SOURCE_SIZE);
-  source_size = fread( source_str, 1, MAX_SOURCE_SIZE, fp);
-  fclose( fp );
   
   /* Get Platform and Device Info */
   ret = clGetPlatformIDs(MAX_DEVICES, platform_ids, &ret_num_platforms);
@@ -281,30 +152,9 @@ int main(){
     check_status("clCreateCommandQueueWithProperties", ret);
   }
 
-  /* Create Kernel Program from the source */
-  program = clCreateProgramWithSource(context, 1, (const char **)&source_str,
-				      (const size_t *)&source_size, &ret);
-  check_status("clCreateProgramWithSource", ret);
-
-  /* Build Kernel Program */
-  ret = clBuildProgram(program, 1, device, NULL, NULL, NULL);
-  if(ret == CL_BUILD_PROGRAM_FAILURE){
-    char *build_log;
-    size_t log_size;
-    clGetProgramBuildInfo(program, *device, CL_PROGRAM_BUILD_LOG,
-			  0, NULL, &log_size);
-    build_log = malloc(log_size+1);
-    clGetProgramBuildInfo(program, *device, CL_PROGRAM_BUILD_LOG,
-			  log_size, build_log, NULL);
-    build_log[log_size] = '\0';
-    fprintf(stderr, "%s\n", build_log);
-    exit(1);
-  }
-  check_status("clBuildProgram", ret);
-
   /* Create OpenCL Kernel */
-  kernel = clCreateKernel(program, "continuity_code", &ret);
-  check_status("clCreateKernel", ret);
+  kernel = build_kernel(&context, device,
+			"./continuity_kern.c", "continuity_code");
 
   /* Create Device Memory Buffers */
   buff_size = nx*ny*sizeof(cl_double);
@@ -475,8 +325,6 @@ int main(){
 
   ret = clReleaseCommandQueue(command_queue);
   ret = clReleaseContext(context);
-
-  free(source_str);
 
 }
 
