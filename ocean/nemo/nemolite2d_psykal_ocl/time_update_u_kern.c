f4fc198220364d4eda7103b3ba8947c2aad2d6cc
Andrew Porter <andrew.porter@stfc.ac.uk>
2018-01-31 14:37:25 +0000

    #1 WIP on two-kernel case with channels

---
+++ ocean/nemo/nemolite2d_psykal_ocl/time_update_u_kern.c
@@ -0,0 +1,100 @@
+#ifndef __OPENCL_VERSION__
+#include <stdio.h>
+#include "opencl_utils.h"
+
+#ifdef __APPLE__
+#include <OpenCL/opencl.h>
+#else
+#include <CL/cl.h>
+#endif
+
+void set_args_next_sshu(cl_kernel kern,
+			cl_int *nx,
+			cl_mem *sshn_u_device,
+			cl_mem *sshn_device,
+			cl_mem *tmask_device,
+			cl_mem *e12t_device,
+			cl_mem *e12u_device){
+  cl_int ret;
+  int arg_idx = 0;
+  ret = clSetKernelArg(kern, arg_idx++, sizeof(cl_int), (void *)nx);
+  check_status("clSetKernelArg", ret);
+  ret = clSetKernelArg(kern, arg_idx++, sizeof(cl_mem), (void *)sshn_u_device);
+  check_status("clSetKernelArg", ret);
+  ret = clSetKernelArg(kern, arg_idx++, sizeof(cl_mem), (void *)sshn_device);
+  check_status("clSetKernelArg", ret);
+  ret = clSetKernelArg(kern, arg_idx++, sizeof(cl_mem), (void *)tmask_device);
+  check_status("clSetKernelArg", ret);
+  ret = clSetKernelArg(kern, arg_idx++, sizeof(cl_mem), (void *)e12t_device);
+  check_status("clSetKernelArg", ret);
+  ret = clSetKernelArg(kern, arg_idx++, sizeof(cl_mem), (void *)e12u_device);
+  check_status("clSetKernelArg", ret);
+
+  fprintf(stdout, "Set %d arguments for next_sshu kernel\n", arg_idx);
+}
+
+#endif
+
+/*
+  type, extends(kernel_type) :: next_sshu
+     type(arg), dimension(5) :: meta_args =  &
+          (/ arg(READWRITE, CU, POINTWISE),  &
+             arg(READ,      CU, POINTWISE),  &
+             arg(READ,      GRID_MASK_T),    &
+             arg(READ,      GRID_AREA_T),    &
+             arg(READ,      GRID_AREA_U)     &
+           /)
+
+     !> We update only those points within the internal region
+     !! of the simulated domain.
+     integer :: ITERATES_OVER = INTERNAL_PTS
+
+     !> Although the staggering of variables used in an Arakawa
+     !! C grid is well defined, the way in which they are indexed is
+     !! an implementation choice. This can be thought of as choosing
+     !! which grid-point types have the same (i,j) index as a T
+     !! point. This kernel assumes that the U,V and F points that
+     !! share the same index as a given T point are those immediately
+     !! to the North and East of it.
+     integer :: index_offset = OFFSET_NE
+
+  contains
+    procedure, nopass :: code => next_sshu_code
+  end type next_sshu
+*/
+
+#ifdef __OPENCL_VERSION__
+__kernel void next_sshu_code(int width,
+			     __global double* restrict sshn_u,
+			     __global double* restrict sshn,
+			     __global int* restrict tmask,
+			     __global double* restrict e12t,
+			     __global double* restrict e12u){
+  int ji = get_global_id(0);
+  int jj = get_global_id(1);
+#else
+  void next_sshu_code(int ji, int jj, int width,
+		      double* sshn_u,
+		      double* sshn,
+		      int* tmask,
+		      double* e12t,
+		      double* e12u){
+#endif
+  double rtmp1;
+  int idx = jj*width + ji;
+  int idxip1 = idx + 1;
+
+  if(tmask[idx] + tmask[idxip1] <= 0)return; // jump over non-computational domain
+
+  if(tmask[idx] * tmask[idxip1] > 0){
+    rtmp1 = e12t[idx] * sshn[idx] + e12t[idxip1] * sshn[idxip1];
+    sshn_u[idx] = 0.5 * rtmp1 / e12u[idx] ;
+  }
+  else if(tmask[idx] <= 0){
+    sshn_u[idx] = sshn[idxip1];
+  }
+  else if(tmask[idxip1] <= 0){
+      sshn_u[idx] = sshn[idx];
+  }
+
+}
