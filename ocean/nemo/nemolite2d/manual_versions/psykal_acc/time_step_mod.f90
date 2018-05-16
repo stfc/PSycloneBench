765941a5acc55911c6336bc6b06166e52d5de0f4
Andrew Porter <andrew.porter@stfc.ac.uk>
2018-05-16 16:16:38 +0100

    Split OpenACC version into two invokes

--- ocean/nemo/nemolite2d/manual_versions/psykal_acc/time_step_mod.f90
+++ ocean/nemo/nemolite2d/manual_versions/psykal_acc/time_step_mod.f90
@@ -13,14 +13,36 @@ contains
                               hu, hv, ht, ua, va, un, vn)
     !use dl_timer
     use field_mod
-    use grid_mod
-    use model_mod,       only: rdt, cbfr, visc
-    use physical_params_mod, only: g, omega, d2r
+!    use grid_mod
+!    use model_mod,       only: rdt, cbfr, visc
+!    use physical_params_mod, only: g, omega, d2r
 !    use momentum_mod,    only: momentum_v_code
-    use momentum_mod,    only: momentum_u_code
+!    use momentum_mod,    only: momentum_u_code
 !    use continuity_mod,  only: continuity_code
 !    use time_update_mod, only: next_sshu_code, next_sshv_code
 !    use boundary_conditions_mod
+    implicit none
+    integer,         intent(in)    :: istp
+    type(r2d_field), intent(inout) :: un, vn, sshn_t, sshn_u, sshn_v
+    type(r2d_field), intent(inout) :: ua, va, ssha, ssha_u, ssha_v
+    type(r2d_field), intent(in)    :: hu, hv, ht
+
+    call invoke_time_step1(istp, ssha, ssha_u, ssha_v, &
+                              sshn_t, sshn_u, sshn_v, &
+                              hu, hv, ht, ua, va, un, vn)
+
+    call invoke_time_step2(istp, ssha, ssha_u, ssha_v, &
+                              sshn_t, sshn_u, sshn_v, &
+                              hu, hv, ht, ua, va, un, vn)
+
+  end subroutine invoke_time_step
+
+  subroutine invoke_time_step1(istp, ssha, ssha_u, ssha_v, &
+                              sshn_t, sshn_u, sshn_v, &
+                              hu, hv, ht, ua, va, un, vn)
+    use field_mod
+    use grid_mod
+    use model_mod,       only: rdt
     implicit none
     integer,         intent(in)    :: istp
     type(r2d_field), intent(inout) :: un, vn, sshn_t, sshn_u, sshn_v
@@ -30,19 +52,6 @@ contains
     integer :: ji, jj, jiu, jiv
     integer :: M, N
     integer :: cont_timer, mom_timer, bc_timer, next_timer
-    ! Locals for momentum
-    REAL(wp) :: u_e, u_w, v_n, v_s
-    real(wp) :: v_nc, v_sc
-    real(wp) :: depe, depw, deps, depn
-    real(wp) :: hpg, adv, cor, vis
-    real(wp) :: dudx_e, dudx_w, dudy_s, dudy_n
-    real(wp) :: uu_e, uu_n, uu_s, uu_w
-    real(wp) :: u_ec, u_wc, vv_e, vv_n, vv_s, vv_w
-    real(wp) :: dvdx_e, dvdx_w, dvdy_n, dvdy_s
-    real(wp) :: rtmp1, rtmp2, rtmp3, rtmp4
-    ! end locals for momentum
-    ! Locals for BCs
-    real(wp) :: amp_tide, omega_tide, rtime
 
     M  = ssha%grid%simulation_domain%xstop
     N  = ssha%grid%simulation_domain%ystop
@@ -52,17 +61,31 @@ contains
 !    do jj = ssha%internal%ystart, ssha%internal%ystop, 1
 !      do ji = ssha%internal%xstart, ssha%internal%xstop, 1
 ! Copy data to GPU. We use pcopyin so that if the data is already
-! on the GPU then that copy is used.
+    ! on the GPU then that copy is used.
+    if(.not. ssha%data_on_device .or. &
+       .not. sshn_t%data_on_device .or. &
+       .not. sshn_u%data_on_device .or. &
+       .not. sshn_v%data_on_device .or. &
+       .not. hu%data_on_device .or. &
+       .not. hv%data_on_device .or. &
+       .not. un%data_on_device .or. &
+       .not. vn%data_on_device)then
 !$acc enter data if(.not. ssha%data_on_device) &
 !$acc copyin(sshn_t, sshn_t%data, sshn_u, sshn_u%data, sshn_v, sshn_v%data, &
-!$acc        ssha, ssha%data, ssha_u, ssha_u%data,  &
+!$acc        ssha, ssha%data,  &
 !$acc        hu, hu%data, hv, hv%data, ht, ht%data, &
-!$acc        ua, ua%data, un, un%data, vn, vn%data, &
-!$acc        sshn_t%grid, sshn_t%grid%tmask, sshn_t%grid%area_t, &
-!$acc        sshn_t%grid%area_u, sshn_t%grid%dx_u, sshn_t%grid%dx_v,   &
-!$acc        sshn_t%grid%dx_t, sshn_t%grid%dy_u, sshn_t%grid%dy_t,   &
-!$acc        sshn_t%grid%gphiu, rdt, cbfr, visc)
-
+!$acc        un, un%data, vn, vn%data, &
+!$acc        sshn_t%grid, sshn_t%grid%tmask, sshn_t%grid%area_t)
+       ssha%data_on_device = .true.
+       sshn_t%data_on_device = .true.
+       sshn_u%data_on_device = .true.
+       sshn_v%data_on_device = .true.
+       hu%data_on_device = .true.
+       hv%data_on_device = .true.
+       un%data_on_device = .true.
+       vn%data_on_device = .true.
+    end if
+    
 !$acc parallel default(present)
 !$acc loop collapse(2)
     do jj = 2, N, 1
@@ -77,6 +100,67 @@ contains
     end do
 !$acc end parallel
 
+    !call timer_stop(cont_timer)
+
+  end subroutine invoke_time_step1
+
+  subroutine invoke_time_step2(istp, ssha, ssha_u, ssha_v, &
+                                sshn_t, sshn_u, sshn_v, &
+                                hu, hv, ht, ua, va, un, vn)
+    use field_mod
+    use grid_mod
+    use model_mod,       only: rdt, cbfr, visc
+    use momentum_mod,    only: momentum_u_code
+    implicit none
+    integer,         intent(in)    :: istp
+    type(r2d_field), intent(inout) :: un, vn, sshn_t, sshn_u, sshn_v
+    type(r2d_field), intent(inout) :: ua, va, ssha, ssha_u, ssha_v
+    type(r2d_field), intent(in)    :: hu, hv, ht
+    ! Locals
+    integer :: ji, jj, jiu, jiv
+    integer :: M, N
+    integer :: cont_timer, mom_timer, bc_timer, next_timer
+
+    M  = ua%grid%simulation_domain%xstop
+    N  = ua%grid%simulation_domain%ystop
+
+    !call timer_start(cont_timer, label='Momentum-u')
+
+!    do jj = ssha%internal%ystart, ssha%internal%ystop, 1
+!      do ji = ssha%internal%xstart, ssha%internal%xstop, 1
+! Copy data to GPU. We use pcopyin so that if the data is already
+! on the GPU then that copy is used.
+    if(.not. ua%data_on_device .or. &
+       .not. un%data_on_device .or. &
+       .not. vn%data_on_device .or. &
+       .not. hu%data_on_device .or. &
+       .not. hv%data_on_device .or. &
+       .not. ht%data_on_device .or. &
+       .not. ssha_u%data_on_device .or. &
+       .not. sshn_t%data_on_device .or. &
+       .not. sshn_u%data_on_device .or. &
+       .not. sshn_v%data_on_device)then
+!$acc enter data &
+!$acc copyin(sshn_t, sshn_t%data, sshn_u, sshn_u%data, sshn_v, sshn_v%data, &
+!$acc        ssha_u, ssha_u%data,  &
+!$acc        hu, hu%data, hv, hv%data, ht, ht%data, &
+!$acc        ua, ua%data, un, un%data, vn, vn%data, &
+!$acc        sshn_t%grid, sshn_t%grid%tmask, sshn_t%grid%area_t, &
+!$acc        sshn_t%grid%area_u, sshn_t%grid%dx_u, sshn_t%grid%dx_v,   &
+!$acc        sshn_t%grid%dx_t, sshn_t%grid%dy_u, sshn_t%grid%dy_t,   &
+!$acc        sshn_t%grid%gphiu, rdt, cbfr, visc)
+       ua%data_on_device = .true.
+       un%data_on_device = .true.
+       vn%data_on_device = .true.
+       hu%data_on_device = .true.
+       hv%data_on_device = .true.
+       ht%data_on_device = .true.
+       ssha_u%data_on_device = .true.
+       sshn_t%data_on_device = .true.
+       sshn_u%data_on_device = .true.
+       sshn_v%data_on_device = .true.
+     end if
+     
 !$acc parallel default(present)
 !$acc loop collapse(2)
     do jj = 2, N, 1
@@ -100,16 +184,8 @@ contains
 !$acc end parallel
 
     !call timer_stop(cont_timer)
-    ssha%data_on_device = .true.
-    sshn_t%data_on_device = .true.
-    sshn_u%data_on_device = .true.
-    sshn_v%data_on_device = .true.
-    hu%data_on_device = .true.
-    hv%data_on_device = .true.
-    un%data_on_device = .true.
-    vn%data_on_device = .true.
 
-  end subroutine invoke_time_step
+  end subroutine invoke_time_step2
 
   ! This routine has been 'module in-lined' to check that PGI compiler
   ! can cope with a !$acc routine so long as it's within the same
