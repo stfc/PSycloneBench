b56e55f3c1b16002faa7eb23f5a9c4750035b515
Rupert Ford <rupert.ford@stfc.ac.uk>
2018-05-21 05:27:21 -0700

    issue #10. More routines don't use 'use'.

--- ocean/nemo/nemolite2d_psykal_ompss/psy_split.f90
+++ ocean/nemo/nemolite2d_psykal_ompss/psy_split.f90
@@ -27,12 +27,6 @@ END MODULE psy_gocean2d
 
 SUBROUTINE invoke_0_deref(ssha_t, sshn_t, sshn_u, sshn_v, hu, hv, un, vn, rdt, ua, ht, ssha_u, va, ssha_v, istp, istop, jstop, n, m, grid_area_t, gat_dim1, gat_dim2, grid_area_u, gau_dim1, gau_dim2, grid_area_v, gav_dim1, gav_dim2, grid_tmask, gtm_dim1, gtm_dim2, grid_dx_u, gxu_dim1, gxu_dim2, grid_dx_v, gxv_dim1, gxv_dim2, grid_dx_t, gxt_dim1, gxt_dim2, grid_dy_u, gyu_dim1, gyu_dim2, grid_dy_v, gyv_dim1, gyv_dim2, grid_dy_t, gyt_dim1, gyt_dim2, grid_gphiu, gphiu_dim1, gphiu_dim2, grid_gphiv, gphiv_dim1, gphiv_dim2)
   
-  USE boundary_conditions_mod, ONLY: bc_flather_v_code
-  USE boundary_conditions_mod, ONLY: bc_flather_u_code
-  USE boundary_conditions_mod, ONLY: bc_solid_v_code
-  USE boundary_conditions_mod, ONLY: bc_solid_u_code
-  USE boundary_conditions_mod, ONLY: bc_ssh_code
-  USE momentum_mod, ONLY: momentum_v_code
   USE momentum_mod, ONLY: momentum_u_code
   USE continuity_mod, ONLY: continuity_code
   IMPLICIT NONE
@@ -76,32 +70,32 @@ SUBROUTINE invoke_0_deref(ssha_t, sshn_t, sshn_u, sshn_v, hu, hv, un, vn, rdt, u
   END DO
       DO j=2,jstop-1
         DO i=2,istop
-          CALL momentum_v_code(i, j, va, un, vn, hu, hv, ht, ssha_v, sshn_t, sshn_u, sshn_v, grid_tmask, grid_dx_v, grid_dx_t, grid_dy_u, grid_dy_v, grid_dy_t, grid_area_v, grid_gphiv)
+          CALL momentum_v_code_wrap(i, j, va, size(va,1), size(va,2), un, size(un,1), size(un,2), vn, size(vn,1), size(vn,2), hu, size(hu,1), size(hu,2), hv, size(hv,1), size(hv,2), ht, size(ht,1), size(ht,2), ssha_v, size(ssha_v,1), size(ssha_v,2), sshn_t, size(sshn_t,1), size(sshn_t,2), sshn_u, size(sshn_u,1), size(sshn_u,2), sshn_v, size(sshn_v,1), size(sshn_v,2), grid_tmask, size(grid_tmask,1), size(grid_tmask,2), grid_dx_v, size(grid_dx_v,1), size(grid_dx_v,2), grid_dx_t, size(grid_dx_t,1), size(grid_dx_t,2), grid_dy_u, size(grid_dy_u,1), size(grid_dy_u,2), grid_dy_v, size(grid_dy_v,1), size(grid_dy_v,2), grid_dy_t, size(grid_dy_t,1), size(grid_dy_t,2), grid_area_v, size(grid_area_v,1), size(grid_area_v,2), grid_gphiv, size(grid_gphiv,1), size(grid_gphiv,2))
         END DO 
       END DO 
       DO j=2,jstop
         DO i=2,istop
-          CALL bc_ssh_code(i, j, istp, ssha_t, grid_tmask)
+          CALL bc_ssh_code_wrap(i, j, istp, ssha_t, size(ssha_t,1), size(ssha_t,2), grid_tmask, size(grid_tmask,1), size(grid_tmask,2))
         END DO 
       END DO 
       DO j=1,jstop+1
         DO i=1,istop
-          CALL bc_solid_u_code(i, j, ua, grid_tmask)
+          CALL bc_solid_u_code_wrap(i, j, ua, size(ua,1), size(ua,2), grid_tmask, size(grid_tmask,1), size(grid_tmask,2))
         END DO 
       END DO 
       DO j=1,jstop
         DO i=1,istop+1
-          CALL bc_solid_v_code(i, j, va, grid_tmask)
+          CALL bc_solid_v_code_wrap(i, j, va, size(va,1), size(va,2), grid_tmask, size(grid_tmask,1), size(grid_tmask,2))
         END DO 
       END DO 
       DO j=1,jstop+1
         DO i=1,istop
-          CALL bc_flather_u_code(i, j, ua, hu, sshn_u, grid_tmask)
+          CALL bc_flather_u_code_wrap(i, j, ua, size(ua,1), size(ua,2), hu, size(hu,1), size(hu,2), sshn_u, size(sshn_u,1), size(sshn_u,2), grid_tmask, size(grid_tmask,1), size(grid_tmask,2))
         END DO 
       END DO 
       DO j=1,jstop
         DO i=1,istop+1
-          CALL bc_flather_v_code(i, j, va, hv, sshn_v, grid_tmask)
+          CALL bc_flather_v_code_wrap(i, j, va, size(va,1), size(va,2), hv, size(hv,1), size(hv,2), sshn_v, size(sshn_v,1), size(sshn_v,2), grid_tmask, size(grid_tmask,1), size(grid_tmask,2))
         END DO 
       END DO 
       DO j=1,jstop+1
@@ -130,6 +124,104 @@ SUBROUTINE invoke_0_deref(ssha_t, sshn_t, sshn_u, sshn_v, hu, hv, un, vn, rdt, u
         END DO 
       END DO 
 END SUBROUTINE invoke_0_deref
+
+subroutine momentum_v_code_wrap(i, j, va, va_dim1, va_dim2, un, un_dim1, un_dim2, vn, vn_dim1, vn_dim2, hu, hu_dim1, hu_dim2, hv, hv_dim1, hv_dim2, ht, ht_dim1, ht_dim2, ssha_v, ssha_v_dim1, ssha_v_dim2, sshn_t, sshn_t_dim1, sshn_t_dim2, sshn_u, sshn_u_dim1, sshn_u_dim2, sshn_v, sshn_v_dim1, sshn_v_dim2, grid_tmask, g_dim1, g_dim2, grid_dx_v, dxv_dim1, dxv_dim2, grid_dx_t, dxt_dim1, dxt_dim2, grid_dy_u, dyu_dim1, dyu_dim2, grid_dy_v, dyv_dim1, dyv_dim2, grid_dy_t, dyt_dim1, dyt_dim2, grid_area_v, gav_dim1, gav_dim2, grid_gphiv, gphiv_dim1, gphiv_dim2)
+  USE momentum_mod, ONLY: momentum_v_code
+  INTEGER, PARAMETER      :: wp = SELECTED_REAL_KIND(12,307)
+  integer, intent(in)     :: i, j
+  integer, intent(in)     :: va_dim1, va_dim2, un_dim1, un_dim2, vn_dim1, vn_dim2, hu_dim1, hu_dim2, hv_dim1, hv_dim2, ht_dim1, ht_dim2, ssha_v_dim1, ssha_v_dim2, sshn_t_dim1, sshn_t_dim2, sshn_u_dim1, sshn_u_dim2, sshn_v_dim1, sshn_v_dim2, g_dim1, g_dim2, dxv_dim1, dxv_dim2, dxt_dim1, dxt_dim2, dyu_dim1, dyu_dim2, dyv_dim1, dyv_dim2, dyt_dim1, dyt_dim2, gav_dim1, gav_dim2, gphiv_dim1, gphiv_dim2
+  real(wp), intent(inout) :: va(va_dim1, va_dim2)
+  real(wp), intent(in)    :: un(un_dim1, un_dim2)
+  real(wp), intent(in)    :: vn(vn_dim1, vn_dim2)
+  real(wp), intent(in)    :: hu(hu_dim1, hu_dim2)
+  real(wp), intent(in)    :: hv(hv_dim1, hv_dim2)
+  real(wp), intent(in)    :: ht(ht_dim1, ht_dim2)
+  real(wp), intent(in)    :: ssha_v(ssha_v_dim1, ssha_v_dim2)
+  real(wp), intent(in)    :: sshn_t(sshn_t_dim1, sshn_t_dim2)
+  real(wp), intent(in)    :: sshn_u(sshn_u_dim1, sshn_u_dim2)
+  real(wp), intent(in)    :: sshn_v(sshn_v_dim1, sshn_v_dim2)
+  integer,  intent(in)    :: grid_tmask(g_dim1, g_dim2)
+  real(wp), intent(in)    :: grid_dx_v(dxv_dim1, dxv_dim2)
+  real(wp), intent(in)    :: grid_dx_t(dxt_dim1, dxt_dim2)
+  real(wp), intent(in)    :: grid_dy_u(dyu_dim1, dyu_dim2)
+  real(wp), intent(in)    :: grid_dy_v(dyv_dim1, dyv_dim2)
+  real(wp), intent(in)    :: grid_dy_t(dyt_dim1, dyt_dim2)
+  real(wp), intent(in)    :: grid_area_v(gav_dim1, gav_dim2)
+  real(wp), intent(in)    :: grid_gphiv(gphiv_dim1, gphiv_dim2)
+  !
+  CALL momentum_v_code(i, j, va, un, vn, hu, hv, ht, ssha_v, sshn_t, sshn_u, sshn_v, grid_tmask, grid_dx_v, grid_dx_t, grid_dy_u, grid_dy_v, grid_dy_t, grid_area_v, grid_gphiv)
+  !
+end subroutine momentum_v_code_wrap
+
+subroutine bc_ssh_code_wrap(i, j, istp, ssha_t, ssh_dim1, ssh_dim2, grid_tmask, g_dim1, g_dim2)
+  USE boundary_conditions_mod, ONLY: bc_ssh_code
+  implicit none
+  INTEGER, PARAMETER      :: wp = SELECTED_REAL_KIND(12,307)
+  integer, intent(in)     :: i, j, istp
+  integer, intent(in)     :: ssh_dim1, ssh_dim2, g_dim1, g_dim2
+  real(wp), intent(inout) :: ssha_t(ssh_dim1, ssh_dim2)
+  integer, intent(in)     :: grid_tmask(g_dim1, g_dim2)
+  !
+  CALL bc_ssh_code(i, j, istp, ssha_t, grid_tmask)
+  !
+end subroutine bc_ssh_code_wrap
+
+subroutine bc_solid_u_code_wrap(i, j, ua, u_dim1, u_dim2, grid_tmask, g_dim1, g_dim2)
+  USE boundary_conditions_mod, ONLY: bc_solid_u_code
+  implicit none
+  INTEGER, PARAMETER      :: wp = SELECTED_REAL_KIND(12,307)
+  integer, intent(in)     :: i, j
+  integer, intent(in)     :: u_dim1, u_dim2, g_dim1, g_dim2
+  real(wp), intent(inout) :: ua(u_dim1, u_dim2)
+  integer, intent(in)     :: grid_tmask(g_dim1, g_dim2)
+  !
+  CALL bc_solid_u_code(i, j, ua, grid_tmask)
+  !
+end subroutine bc_solid_u_code_wrap
+
+
+subroutine bc_solid_v_code_wrap(i, j, va, v_dim1, v_dim2, grid_tmask, g_dim1, g_dim2)
+  USE boundary_conditions_mod, ONLY: bc_solid_v_code
+  implicit none
+  INTEGER, PARAMETER      :: wp = SELECTED_REAL_KIND(12,307)
+  integer, intent(in)     :: i, j
+  integer, intent(in)     :: v_dim1, v_dim2, g_dim1, g_dim2
+  real(wp), intent(inout) :: va(v_dim1, v_dim2)
+  integer, intent(in)     :: grid_tmask(g_dim1, g_dim2)
+  !
+  CALL bc_solid_v_code(i, j, va, grid_tmask)
+  !
+end subroutine bc_solid_v_code_wrap
+
+subroutine bc_flather_u_code_wrap(i, j, ua, u_dim1, u_dim2, hu, hu_dim1, hu_dim2, sshn_u, ssh_dim1, ssh_dim2, grid_tmask, g_dim1, g_dim2)
+  USE boundary_conditions_mod, ONLY: bc_flather_u_code
+  implicit none
+  INTEGER, PARAMETER      :: wp = SELECTED_REAL_KIND(12,307)
+  integer, intent(in)     :: i, j
+  integer, intent(in)     :: u_dim1, u_dim2, hu_dim1, hu_dim2, ssh_dim1, ssh_dim2, g_dim1, g_dim2
+  real(wp), intent(inout) :: ua(u_dim1, u_dim2)
+  real(wp), intent(in)    :: hu(hu_dim1, hu_dim2)
+  real(wp), intent(in)    :: sshn_u(ssh_dim1, ssh_dim2)
+  integer, intent(in)     :: grid_tmask(g_dim1, g_dim2)
+  !
+  CALL bc_flather_u_code(i, j, ua, hu, sshn_u, grid_tmask)
+  !
+end subroutine bc_flather_u_code_wrap
+
+subroutine bc_flather_v_code_wrap(i, j, va, v_dim1, v_dim2, hv, hv_dim1, hv_dim2, sshn_v, ssh_dim1, ssh_dim2, grid_tmask, g_dim1, g_dim2)
+  USE boundary_conditions_mod, ONLY: bc_flather_v_code
+  implicit none
+  INTEGER, PARAMETER      :: wp = SELECTED_REAL_KIND(12,307)
+  integer, intent(in)     :: i, j
+  integer, intent(in)     :: v_dim1, v_dim2, hv_dim1, hv_dim2, ssh_dim1, ssh_dim2, g_dim1, g_dim2
+  real(wp), intent(inout) :: va(v_dim1, v_dim2)
+  real(wp), intent(in)    :: hv(hv_dim1, hv_dim2)
+  real(wp), intent(in)    :: sshn_v(ssh_dim1, ssh_dim2)
+  integer, intent(in)     :: grid_tmask(g_dim1, g_dim2)
+  !
+  CALL bc_flather_v_code(i, j, va, hv, sshn_v, grid_tmask)
+  !
+end subroutine bc_flather_v_code_wrap
 
 subroutine field_copy_code_wrap(i, j, field_out, out_dim1, out_dim2, field_in, in_dim1, in_dim2)
   USE infrastructure_mod, ONLY: field_copy_code
