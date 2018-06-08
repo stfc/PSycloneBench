053ac1ca87c1f5f7da006c6d958528c52611b174
Andrew Porter <andrew.porter@stfc.ac.uk>
2018-06-08 11:40:15 +0100

    #9 use PGI-env script rather than Makefile.include for single_file_acc

--- compiler_setup/pgi_acc.sh
+++ compiler_setup/pgi_acc.sh
@@ -1,9 +1,12 @@
 # Build settings for the PGI compiler with OpenACC
+# ================================================
+# Fortran compiler
 F90=pgf90
-
-F90FLAGS=
-LDFLAGS=
-
+# C compiler
+CC=pgcc
+CFLAGS="-g"
+# Fortran compiler flags
+F90FLAGS="-O3 -Minfo=all"
 #F90FLAGS"+=" -fcheck=all -fbacktrace -ffpe-trap=invalid -g -O0"
 # -Mcuda is for CUDA Fortran
 # nordc - do not link to routines compiled for device (ensure
@@ -12,13 +15,21 @@ LDFLAGS=
 # Registers are shared by threads in an SMP. The more registers a kernel
 # uses, the fewer threads it can support. This parameter can be tuned and
 # shoul be a multiple of 8.
-#F90FLAGS+=" -O3 -acc -ta=tesla:cc30,nordc -Minfo=all"
-#LDFLAGS+=" -O3 -acc -ta=tesla,cc30 -Mcuda=cc30,nordc"
-F90FLAGS+=" -O3 -acc -ta=tesla,cc35,maxregcount:80,nordc -Minfo=all"
-LDFLAGS+=" -O3 -acc -ta=nvidia,cc35 -Mcuda=cc35,nordc"
-
+# -Mcuda is required to build CUDA Fortran
+# For Quadro K600
+F90FLAGS+=" -acc -ta=tesla:cc30,nordc -Mcuda=cc30,nordc"
+# For Tesla K20c
+#F90FLAGS+=" -acc -ta=tesla,cc35,maxregcount:80,nordc -Mcuda=cc35,maxregcount:80,nordc"
+# Linker flags
+# For Quadro K600
+LDFLAGS+=" -acc -ta=tesla,cc30 -Mcuda=cc30,nordc"
+# For Tesla K20c
+#LDFLAGS="-acc -ta=nvidia,cc35 -Mcuda=cc35,nordc"
+# Flags to use when compiling with OpenMP support
+OMPFLAGS="-mp"
+# Command to use to create archive of object files
 AR=ar
-
+# ==============================
 export F90
 export F90FLAGS
 export LDFLAGS
