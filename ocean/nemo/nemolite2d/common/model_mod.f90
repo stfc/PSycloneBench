162ed37351b0bda570b6569f80f86cade0997080
Andrew Porter <andrew.porter@stfc.ac.uk>
2018-07-27 14:53:33 +0100

    #7 make dm version use modules in common dir

--- ocean/nemo/nemolite2d/common/model_mod.f90
+++ ocean/nemo/nemolite2d/common/model_mod.f90
@@ -12,11 +12,11 @@ MODULE model_mod
   !> type (source?) of grid
   integer, save  :: jphgr_msh
 
-  real(wp), target, save :: rdt             !< time step
-  REAL(wp), target, save :: cbfr            !< bottom friction coefficient
-  REAL(wp), target, save :: visc            !< backgroud/constant viscosity 
+  real(go_wp), save :: rdt             !< time step
+  REAL(go_wp), save :: cbfr            !< bottom friction coefficient
+  REAL(go_wp), save :: visc            !< backgroud/constant viscosity 
 
-  REAL(wp), save :: dep_const       !< constant depth
+  REAL(go_wp), save :: dep_const       !< constant depth
 
   
 CONTAINS
@@ -25,14 +25,19 @@ CONTAINS
 
   subroutine model_init(grid)
     use gocean2d_io_mod
+    use decomposition_mod, only: decomposition_type
+    use subdomain_mod, only: decompose
+    use parallel_mod, only: get_rank, set_proc_grid, map_comms
     implicit none
     type(grid_type), intent(inout) :: grid
 
-    !> Problem size, read from namelist
+    !> Global problem size, read from namelist
     integer :: jpiglo, jpjglo
-    real(wp) :: dx, dy
+    real(go_wp) :: dx, dy
     integer  :: ierr
     integer, dimension(:,:), allocatable :: tmask
+    type(decomposition_type) :: decomp
+    integer :: rank
 
     ! Initialise timing system
     call timer_init()
@@ -42,16 +47,22 @@ CONTAINS
                        nit000, nitend, irecord, &
                        jphgr_msh, dep_const, rdt, cbfr, visc)
 
-    ! Set-up the T mask. This defines the model domain.
-    allocate(tmask(jpiglo,jpjglo), stat=ierr)
-    if(ierr /= 0)then
-       stop 'model_init: failed to allocate T mask'
-    end if
+    ! Work out the decomposition of the global domain (uses the number
+    ! of MPI ranks by default)
+    decomp = decompose(jpiglo, jpjglo)
+    call set_proc_grid(decomp%nx, decomp%ny)
 
-    call setup_tpoints_mask(jpiglo, jpjglo, tmask)
+    ! Set-up the T mask on this process. This defines the model domain.
+    rank = get_rank()
+    call setup_tpoints_mask(decomp%subdomains(rank), jpiglo, jpjglo, tmask)
 
     ! Having specified the T points mask, we can set up mesh parameters
-    call grid_init(grid, jpiglo, jpjglo, dx, dy, tmask)
+    call grid_init(grid, decomp, dx, dy, tmask)
+
+    ! Write generated, decomposed T-mask out to file
+    call dump_tmask(tmask, grid)
+
+    call map_comms(decomp, tmask, .false., ierr)
 
     ! Initialise model IO 'system'
     call model_write_init(jpiglo, jpjglo, irecord)
@@ -67,12 +78,12 @@ CONTAINS
     use gocean2d_io_mod, only: model_write_finalise
     IMPLICIT none
 
-    CALL model_write_finalise()
+    call model_write_finalise()
 
-    CALL timer_report()
+    call timer_report()
 
-    CALL model_dealloc()
-  
+    call model_dealloc()
+
   end subroutine model_finalise
 
   !================================================
@@ -84,12 +95,24 @@ CONTAINS
 
   !================================================
 
-  subroutine setup_tpoints_mask(jpi, jpj, tmask)
+  subroutine setup_tpoints_mask(subdomain, jpi, jpj, tmask)
+    !> Allocate and set-up the T-points mask that defines the
+    !! simulation domain on the current process.
+    use gocean_mod, only: gocean_stop
+    use decomposition_mod, only: subdomain_type
     implicit none
+    ! The (sub)domain dealt with by the calling process
+    type(subdomain_type), intent(in) :: subdomain
+    ! Dimensions of global domain
     integer, intent(in) :: jpi, jpj
-    integer, dimension(:,:), intent(inout) :: tmask
+    integer, dimension(:,:), allocatable, intent(inout) :: tmask
     ! Locals
-    integer :: ji, jj
+    integer :: ji, jj, ierr
+
+    allocate(tmask(subdomain%global%nx, subdomain%global%ny), stat=ierr)
+    if(ierr /= 0)then
+       call gocean_stop('model_init: failed to allocate T mask')
+    end if
 
     ! jphgr_msh is read from the namelist file
     !jphgr_msh = 0    ! read in grid definition from file
@@ -100,7 +123,8 @@ CONTAINS
     case(0) ! read in grid from a coordinate file
 
        ! to be added
-       stop "Reading the grid definition from file is not yet supported!"
+       call gocean_stop("Reading the grid definition from file is "// &
+                        "not yet supported!")
        ! add reading data part here
        ! the following variables/arrays are needed:
        ! jpi, jpj, 
@@ -123,28 +147,71 @@ CONTAINS
 
        ! Define Model solid/open Boundaries via the properties of t-cells
 
-       tmask(:,:) = 1                 ! all inner cells
+       tmask(:,:) = -1 ! Default all cells to being outside the domain
 
+       ! Mark all inner cells
+       tmask(subdomain%internal%xstart:subdomain%internal%xstop, &
+             subdomain%internal%ystart:subdomain%internal%ystop) = 1
+             
        ! -define solid/open boundaries
-       DO jj = 1, jpj
-          tmask(  1, jj) = 0              ! west solid boundary
-          tmask(jpi, jj) = 0              ! east solid boundary
-       END Do
+       if(subdomain%global%xstart == 1)then
+          do jj = 1, subdomain%internal%ny
+             tmask(subdomain%internal%xstart, jj) = 0 ! west solid boundary
+          end do
+       end if
 
-       DO ji = 1, jpi
-          tmask(ji,jpj) = 0               ! north solid boundary
-       END Do
+       if(subdomain%global%xstop == jpi)then
+          do jj = 1, subdomain%internal%ny
+             tmask(subdomain%internal%xstop, jj) = 0 ! east solid boundary
+          end do
+       end if
 
-       DO ji = 1, jpi
-          tmask(ji,1) = -1                    ! south open boundary
-       END Do
+       if(subdomain%global%ystop == jpj)then
+          do ji = 1, subdomain%internal%nx
+             tmask(ji, subdomain%internal%ystop) = 0 ! north solid boundary
+          end do
+       end if
 
-    CASE DEFAULT
+       if(subdomain%global%ystart == 1)then
+          do ji = 1, subdomain%internal%nx
+             tmask(ji, subdomain%internal%ystart) = -1 ! south open boundary
+          end do
+       end if
+
+    case DEFAULT
        ! undefined jphgr_msh value
        ! add interrupt here
-       STOP "Wrong grid definition type, check your setup !!!!"
-    END SELECT
+       call gocean_stop("Wrong grid definition type (jphgr_msh must be 0 "// &
+                        "or 1), check your namelist file.")
+    end select
 
   end subroutine setup_tpoints_mask
+
+  subroutine dump_tmask(tmask, grid)
+    use parallel_mod, only: get_rank
+    use grid_mod, only: grid_type
+    implicit none
+    type(grid_type), intent(in) :: grid
+    integer, dimension(:,:), intent(in) :: tmask
+    ! Locals
+    integer :: ji, jj
+    character(len=20) :: fname
+
+    write(fname, '(I5.5)') get_rank()
+    open(21, file='tmask_'//TRIM(fname)//'.dat', STATUS='UNKNOWN', &
+         action='write')
+
+    ! Loop over 'internal' T points
+    DO jj = grid%subdomain%internal%ystart, grid%subdomain%internal%ystop, 1
+       DO ji = grid%subdomain%internal%xstart, grid%subdomain%internal%xstop, 1
+
+          write(21,'(2e16.7,1x,I3)') grid%xt(ji,jj), grid%yt(ji,jj), &
+               tmask(ji,jj)
+       END DO
+       WRITE(21,*)
+    END DO
+          
+    close(21)
+  end subroutine dump_tmask
 
 END MODULE model_mod
