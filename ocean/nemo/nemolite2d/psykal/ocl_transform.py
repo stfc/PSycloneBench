a2cba426974643344d8bfb918342504243540fa1
Sergi Siso <sergiesg@gmail.com>
2020-01-22 13:50:31 +0000

    Update psyclone script and continuity kernel to apply the GlobalsToArguments transformation

--- ocean/nemo/nemolite2d/psykal/ocl_transform.py
+++ ocean/nemo/nemolite2d/psykal/ocl_transform.py
@@ -5,16 +5,25 @@ def trans(psy):
 
     from psyclone.psyGen import TransInfo
     tinfo = TransInfo()
+    globaltrans = tinfo.get_trans_name('KernelGlobalsToArguments')
     cltrans = tinfo.get_trans_name('OCLTrans')
     verbose = False
 
     # First invoke
     schedule = psy.invokes.get('invoke_0').schedule
-    cltrans.apply(schedule)
 
     # Map of OpenCL options
     ocl_options =  {'local_size': 4}
 
+    # Remove the globals from inside each kernel
+    for idx, kern in enumerate(schedule.kernels()):
+        print(kern.name)
+        globaltrans.apply(kern)
+
+    cltrans.apply(schedule)
+
+
+    return psy
     # Attach options object to Kernel Call and Kernel Schedule
     for idx, kern in enumerate(schedule.kernels()):
         kern.set_opencl_options(ocl_options)
