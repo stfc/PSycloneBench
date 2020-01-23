75d768dc71692626b55877e96d23eee6c4e6e3f4
Sergi Siso <sergiesg@gmail.com>
2020-01-23 11:03:36 +0000

    Simplify ocl_transform psyclone script

--- ocean/nemo/nemolite2d/psykal/ocl_transform.py
+++ ocean/nemo/nemolite2d/psykal/ocl_transform.py
@@ -7,31 +7,15 @@ def trans(psy):
     tinfo = TransInfo()
     globaltrans = tinfo.get_trans_name('KernelGlobalsToArguments')
     cltrans = tinfo.get_trans_name('OCLTrans')
-    verbose = False
 
-    # First invoke
+    # Get the invoke
     schedule = psy.invokes.get('invoke_0').schedule
-
-    # Map of OpenCL options
-    ocl_options =  {'local_size': 4}
 
     # Remove the globals from inside each kernel
     for idx, kern in enumerate(schedule.kernels()):
-        print(kern.name)
         globaltrans.apply(kern)
 
+    # Transform invoke to OpenCL
     cltrans.apply(schedule)
-
-
-    return psy
-    # Attach options object to Kernel Call and Kernel Schedule
-    for idx, kern in enumerate(schedule.kernels()):
-        kern.set_opencl_options(ocl_options)
-        kschedule = kern.get_kernel_schedule()
-        if verbose:
-            print("")
-            kschedule.symbol_table.view()
-            kschedule.view()
-        kschedule.opencl_options = ocl_options
 
     return psy
