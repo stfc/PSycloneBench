3fb1c08cc19f5f9fc53d53b2b87aaae140d927ff
Sergi Siso <sergiesg@gmail.com>
2020-03-12 11:18:44 +0000

    Create files for xilinx compiler setup

---
+++ compiler_setup/reset.sh
@@ -0,0 +1,9 @@
+# Reset PSycloneBench compiler flags - useful to do before loading a new
+# compiler without leaving hanging previous environment variables.
+unset CC
+unset F90
+unset F90FLAGS
+unset OMPFLAGS
+unset LDFLAGS
+unset AR
+
