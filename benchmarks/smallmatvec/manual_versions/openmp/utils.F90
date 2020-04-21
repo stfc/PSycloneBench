088505eab5b88db61f7b7c6f758c39c72e935494
Sergi Sisó <sergiesg@gmail.com>
2020-04-21 13:23:11 +0100

    Updated utils.F90 and described its limitations

--- benchmarks/smallmatvec/manual_versions/openmp/utils.F90
+++ benchmarks/smallmatvec/manual_versions/openmp/utils.F90
@@ -1,45 +1,47 @@
 module utils
     implicit none
     contains
-subroutine system_mem_usage(valueRSS)
-    use ifport !if on intel compiler
-    implicit none
-    integer, intent(out) :: valueRSS
 
-    character(len=200):: filename=' '
-    character(len=80) :: line
-    character(len=8)  :: pid_char=' '
-    integer :: pid
-    logical :: ifxst
+    ! This function has only been tested on Linux OS with Intel Compiler.
+    ! It reads the UNIX /proc files, so it is only portable to OSs supporting
+    ! them. getpid() can be used on gfortran without importing any module.
+    subroutine system_mem_usage(valueRSS)
+        use ifport ! Import getpid() on Intel compiler
+        implicit none
+        integer, intent(out) :: valueRSS
 
-    valueRSS=-1    ! return negative number if not found
+        character(len=200):: filename=' '
+        character(len=80) :: line
+        character(len=8)  :: pid_char=' '
+        integer :: pid
+        logical :: ifxst
 
-    !--- get process ID
+        valueRSS=-1    ! return negative number if not found
 
-    pid=getpid()
-    write(pid_char,'(I8)') pid
-    filename='/proc/'//trim(adjustl(pid_char))//'/status'
+        !--- get process ID
+        pid=getpid()
+        write(pid_char,'(I8)') pid
+        filename='/proc/'//trim(adjustl(pid_char))//'/status'
 
-    !--- read system file
+        !--- read system file
+        inquire(file=filename, exist=ifxst)
+        if (.not.ifxst) then
+          write (*,*) 'system file does not exist'
+          return
+        endif
 
-    inquire (file=filename,exist=ifxst)
-    if (.not.ifxst) then
-      write (*,*) 'system file does not exist'
-      return
-    endif
+        open(unit=100, file=filename, action='read')
+        do
+          read (100,'(a)',end=120) line
+          if (line(1:6).eq.'VmRSS:') then
+             read (line(7:),*) valueRSS
+             exit
+          endif
+        enddo
+        120 continue
+        close(100)
 
-    open(unit=100, file=filename, action='read')
-    do
-      read (100,'(a)',end=120) line
-      if (line(1:6).eq.'VmRSS:') then
-         read (line(7:),*) valueRSS
-         exit
-      endif
-    enddo
-    120 continue
-    close(100)
-
-    return
-end subroutine system_mem_usage
+        return
+    end subroutine system_mem_usage
 
 end module utils
