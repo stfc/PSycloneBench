647451fd55f3bc81046a622b2c00d634c629adae
Sergi Sisó <sergiesg@gmail.com>
2020-05-29 10:04:16 +0100

    Experiment with PSyclone opencl transformation in the Shallow benchmark

---
+++ benchmarks/shallow/SEQ/ocl_transform.py
@@ -0,0 +1,51 @@
+def trans(psy):
+    ''' Python script intended to be passed to PSyclone's generate()
+    function via the -s option. Applies OpenCL to the Schedule so
+    that PSyclone will generate an OpenCL PSy layer. '''
+
+    from psyclone.psyGen import TransInfo
+    tinfo = TransInfo()
+    globaltrans = tinfo.get_trans_name('KernelGlobalsToArguments')
+    cltrans = tinfo.get_trans_name('OCLTrans')
+
+    # Get the invoke
+    schedule = psy.invokes.get('invoke_0').schedule
+
+    # Remove the globals from inside each kernel
+    for idx, kern in enumerate(schedule.kernels()):
+        globaltrans.apply(kern)
+
+    # Transform invoke to OpenCL
+    cltrans.apply(schedule)
+
+    # Get the invoke
+    schedule = psy.invokes.get('invoke_1').schedule
+
+    # Remove the globals from inside each kernel
+    for idx, kern in enumerate(schedule.kernels()):
+        globaltrans.apply(kern)
+
+    # Transform invoke to OpenCL
+    cltrans.apply(schedule)
+
+    # Get the invoke
+    schedule = psy.invokes.get('invoke_2').schedule
+
+    # Remove the globals from inside each kernel
+    for idx, kern in enumerate(schedule.kernels()):
+        globaltrans.apply(kern)
+
+    # Transform invoke to OpenCL
+    cltrans.apply(schedule)
+
+    # Get the invoke
+    schedule = psy.invokes.get('invoke_3').schedule
+
+    # Remove the globals from inside each kernel
+    for idx, kern in enumerate(schedule.kernels()):
+        globaltrans.apply(kern)
+
+    # Transform invoke to OpenCL
+    cltrans.apply(schedule)
+
+    return psy
