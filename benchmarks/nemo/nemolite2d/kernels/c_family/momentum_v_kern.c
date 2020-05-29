e2a320479ebe79c48801832210e84bdc94a9e702
Sergi Sisó <sergiesg@gmail.com>
2020-05-29 16:10:15 +0100

    Fix indentation issues

--- benchmarks/nemo/nemolite2d/kernels/c_family/momentum_v_kern.c
+++ benchmarks/nemo/nemolite2d/kernels/c_family/momentum_v_kern.c
@@ -12,23 +12,23 @@
 #endif
 
 void set_args_momv(cl_kernel kern,
-		   cl_int *nx,
-		   cl_mem *va_device,
-		   cl_mem *un_device,
-		   cl_mem *vn_device,
-		   cl_mem *hu_device,
-		   cl_mem *hv_device,
-		   cl_mem *ht_device,
-		   cl_mem *ssha_v_device,
-		   cl_mem *sshn_device,
-		   cl_mem *sshn_u_device,
-		   cl_mem *sshn_v_device,
-		   cl_mem *tmask_device,
-		   cl_mem *e1v_device, cl_mem *e1t_device,
-		   cl_mem *e2u_device, cl_mem *e2v_device,
-		   cl_mem *e2t_device, cl_mem *e12v_device,
-		   cl_mem *gphiv_device,
-		   cl_double *rdt, cl_double *cbfr, cl_double *visc){
+           cl_int *nx,
+           cl_mem *va_device,
+           cl_mem *un_device,
+           cl_mem *vn_device,
+           cl_mem *hu_device,
+           cl_mem *hv_device,
+           cl_mem *ht_device,
+           cl_mem *ssha_v_device,
+           cl_mem *sshn_device,
+           cl_mem *sshn_u_device,
+           cl_mem *sshn_v_device,
+           cl_mem *tmask_device,
+           cl_mem *e1v_device, cl_mem *e1t_device,
+           cl_mem *e2u_device, cl_mem *e2v_device,
+           cl_mem *e2t_device, cl_mem *e12v_device,
+           cl_mem *gphiv_device,
+           cl_double *rdt, cl_double *cbfr, cl_double *visc){
   cl_int ret;
   cl_int arg_idx = 0;
 
@@ -127,37 +127,37 @@ void set_args_momv(cl_kernel kern,
 #ifdef __OPENCL_VERSION__
 /** Interface to OpenCL version of kernel */
 __kernel void momentum_v_code(int width,
-			      __global double* restrict va,
-			      const __global double* restrict un,
-			      const __global double* restrict vn, 
-			      const __global double* restrict hu,
-			      const __global double* restrict hv,
-			      const __global double* restrict ht,
-			      const __global double* restrict ssha_v, 
-			      const __global double* restrict sshn,
-			      const __global double* restrict sshn_u,
-			      const __global double* restrict sshn_v, 
-			      const __global int* restrict tmask,
-			      const __global double* restrict e1v,
-			      const __global double* restrict e1t,
-			      const __global double* restrict e2u,
-			      const __global double* restrict e2v,
-			      const __global double* restrict e2t,
-			      const __global double* restrict e12v,
-			      const __global double* restrict gphiv,
-			      double rdt, double cbfr, double visc,
+                  __global double* restrict va,
+                  const __global double* restrict un,
+                  const __global double* restrict vn, 
+                  const __global double* restrict hu,
+                  const __global double* restrict hv,
+                  const __global double* restrict ht,
+                  const __global double* restrict ssha_v, 
+                  const __global double* restrict sshn,
+                  const __global double* restrict sshn_u,
+                  const __global double* restrict sshn_v, 
+                  const __global int* restrict tmask,
+                  const __global double* restrict e1v,
+                  const __global double* restrict e1t,
+                  const __global double* restrict e2u,
+                  const __global double* restrict e2v,
+                  const __global double* restrict e2t,
+                  const __global double* restrict e12v,
+                  const __global double* restrict gphiv,
+                  double rdt, double cbfr, double visc,
                   double omega, double d2r, double g){
   int ji = get_global_id(0);
   int jj = get_global_id(1);
 #else
 /** Interface to standard C version of kernel */
 inline void momentum_v_code(int ji, int jj, int width,
-		     double *va, double *un, double *vn, 
-		     double *hu, double *hv, double *ht, double *ssha_v, 
-		     double *sshn, double *sshn_u, double *sshn_v, 
-		     int *tmask, double *e1v, double *e1t, double *e2u,
-		     double *e2v, double *e2t, double *e12v, double *gphiv,
-		     double rdt, double cbfr, double visc,
+             double *va, double *un, double *vn, 
+             double *hu, double *hv, double *ht, double *ssha_v, 
+             double *sshn, double *sshn_u, double *sshn_v, 
+             int *tmask, double *e1v, double *e1t, double *e2u,
+             double *e2v, double *e2t, double *e12v, double *gphiv,
+             double rdt, double cbfr, double visc,
              double omega, double d2r, double g){
 #endif
 
@@ -178,7 +178,7 @@ inline void momentum_v_code(int ji, int jj, int width,
   
 #ifdef SIMPLE_MOMENTUM
   va[idx] = (vn[idx] * (hv[idx] + sshn_v[idx]) + 
-	       rdt * (1.0) / e12v[idx] ) / 
+           rdt * (1.0) / e12v[idx] ) / 
     ((hv[idx] + ssha_v[idx])) / (1.0 + cbfr * rdt) ;
 #else
   idxim1 = idx - 1;
@@ -200,12 +200,12 @@ inline void momentum_v_code(int ji, int jj, int width,
   u_wc = 0.5 * (un[idxim1] + un[idxim1jp1]);
   u_w  = 0.5 * u_wc * (e2u[idxim1] + e2u[idxim1jp1]);
   depw = 0.50 * (hu[idxim1] + sshn_u[idxim1] + 
-		 hu[idxim1jp1] + sshn_u[idxim1jp1]);
+         hu[idxim1jp1] + sshn_u[idxim1jp1]);
 
   u_ec = 0.5 * (un[idx] + un[idxjp1]);
   u_e  = 0.5 * u_ec * (e2u[idx] + e2u[idxjp1]);
   depe = 0.50 * (hu[idx] + sshn_u[idx] + 
-		 hu[idxjp1] + sshn_u[idxjp1]);
+         hu[idxjp1] + sshn_u[idxjp1]);
 
   // -advection (currently first order upwind)
   vv_s = (0.5 - copysign(0.5, v_s)) * vn[idx] +  
@@ -271,7 +271,7 @@ inline void momentum_v_code(int ji, int jj, int width,
 
   // -linear bottom friction (implemented implicitly.
   va[idx] = (vn[idx] * (hv[idx] + sshn_v[idx]) + 
-	       rdt * (adv + vis + cor + hpg) / e12v[idx] ) / 
+           rdt * (adv + vis + cor + hpg) / e12v[idx] ) / 
     ((hv[idx] + ssha_v[idx])) / (1.0 + cbfr * rdt) ;
 #endif
 }
