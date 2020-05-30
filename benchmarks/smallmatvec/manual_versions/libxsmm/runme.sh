ff832c944e2da948f6e078eea8d17d4482614b63
Rupert Ford <rupert.ford@stfc.ac.uk>
2020-05-30 23:16:52 +0100

    issue #30. Updated runme script to output different files for plotting.

--- benchmarks/smallmatvec/manual_versions/libxsmm/runme.sh
+++ benchmarks/smallmatvec/manual_versions/libxsmm/runme.sh
@@ -1,47 +1,50 @@
 #!/usr/bin/env bash
 
-# This script has Intel-specific KMP environment variables in it
+# Note, this script has Intel-specific KMP environment variables in it
 
 # This script executes the selected original and xsmm matrix_vector
 # multiplication implementation with a variety of OpenMP
 # configurations to analyse its performance.
 
+# Update the for statements to configure each parameter search space.
+
 # The configurable parameters are:
-#   - $v: matrix_vector implementation
 #   - $ps: problem size (horizontal)
+#   - $vertical: problem size (vertical)
 #   - $omp_sch: OpenMP scheduler [static, dynamic, guided]
 #   - $omp_aff: OpenMP affinity [none, compact, scatter]
 #   - $smt: Number of SMT
 #   - $cores: Number of cores
-
-# Update the for statements to configure each parameter search space.
+#   - $v: matrix_vector implementation [orig, xsmm]
 
 echo "Don't forget to set the parameters to the required values"
 exit
 
-output=results.txt
+echo "Original vs libxsmm timing results"
 
-echo "Original vs libxsmm plot" | tee $output
-
-for v in orig xsmm ; do
-    for ps in 32; do
-	for vertical in 32 100; do
-            for omp_sch in static ; do
+for ps in 32; do
+    for vertical in 32 100; do
+        for omp_sch in static ; do
             for omp_aff in scatter ;  do
-            for smt in 1 2; do
-            for cores in 1 2 4 6 8; do
-                echo $v $smt $cores
-                OMP_NUM_THREADS=$(( ${cores} * ${smt} )) \
-                    KMP_HW_SUBSET=${cores}c,${smt}t \
-                    KMP_AFFINITY=$omp_aff OMP_SCHEDULE=$omp_sch \
-                    ./kdriver_$v -g $ps $vertical > output
-                t=$(cat output | grep "Loop time" | awk '{print $5}')
-                check=$(cat output | grep "Reduction value" | awk '{print $3}')
-                echo $v $ps $vertical $omp_sch $omp_aff $cores $smt $t $check >> $output
-            done
-            done
-            done
+		for smt in 1 2; do
+		    output=libxsmm_results_h${ps}_v${vertical}_sch${omp_sch}_aff${omp_aff}_smt${smt}.txt
+		    for cores in 1 2 4 6 8; do
+			echo $v $ps $vertical $omp_sch $omp_aff $cores $smt
+			echo -n $v $ps $vertical $omp_sch $omp_aff $cores $smt >> $output
+			for v in orig xsmm ; do
+			    OMP_NUM_THREADS=$(( ${cores} * ${smt} )) \
+			      KMP_HW_SUBSET=${cores}c,${smt}t \
+			      KMP_AFFINITY=$omp_aff OMP_SCHEDULE=$omp_sch \
+			      ./kdriver_$v -g $ps $vertical > output
+			    t=$(cat output | grep "Loop time" | awk '{print $5}')
+			    check=$(cat output | grep "Reduction value" | awk '{print $3}')
+			    echo -n $t $check >> $output
+			done
+			echo >> $output
+		    done
+		done
             done
         done
     done
 done
+exit
