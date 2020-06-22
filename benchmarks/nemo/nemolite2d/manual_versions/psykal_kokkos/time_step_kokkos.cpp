701c2b9a78454ae44b641b1ba335b1b4a401e0e8
Sergi Siso <sergiesg@gmail.com>
2020-06-22 09:36:01 +0100

    NemoLite2D Kokkos implementation: Use views and start inlining the kernels

--- benchmarks/nemo/nemolite2d/manual_versions/psykal_kokkos/time_step_kokkos.cpp
+++ benchmarks/nemo/nemolite2d/manual_versions/psykal_kokkos/time_step_kokkos.cpp
@@ -48,6 +48,7 @@ extern "C" void c_invoke_time_step(
         int internal_ystart,
         int internal_ystop,
         int width,
+        int total_size,
         double rdt,
         double cbfr,
         double visc,
@@ -55,6 +56,13 @@ extern "C" void c_invoke_time_step(
         double d2r,
         double g
         ){
+
+    // MDRangePolicy uses an open interval (does not include the end
+    // point), while the provided 'stop' represent closed ranges.
+    // Therefore we need to increase by 1 the 'stop' values (since they
+    // are passed by values this only affects this function).
+    internal_ystop = internal_ystop + 1;
+    internal_xstop = internal_xstop + 1;
 
     // Kokkos needs to be initialized. Since NemoLite2D just has a single
     // invoke, it is simple to do it here the first time the invoke is
@@ -64,17 +72,6 @@ extern "C" void c_invoke_time_step(
         Kokkos::initialize();
         first_time = false;
     }
-
-    // In this implementation we will use 1D UnmanagedViews as they wrap the
-    // raw pointer passed in the function. This have several limitations as
-    // kokkos does not see the 2D pattern and can't choose the appropriate
-    // data-layout for it.
-    using UnmanagedView = kokkos::View<
-            double*, 
-            Kokkos::HostSpace,
-            Kokkos::MemoryTraits<Kokkos::Unmanaged>>;
-
-    UnmanagedView ssha_t_view(ssh_t, width*width);
 
     // The execution space is given as a preprocessor define when compiling
     // this file. e.g. `g++ -DEXEC_SPACE=OpenMP time_step_kokkos.cpp -c`
@@ -87,31 +84,66 @@ extern "C" void c_invoke_time_step(
     // Execution policy for a multi-dimensional (2D) iteration space.
     typedef Kokkos::MDRangePolicy<Kokkos::Rank<2>, execution_space> mdrange_policy;
 
-    // MDRangePolicy uses an open interval (does not include the end
-    // point), while the provided 'stop' represent closed ranges.
-    // Therefore we need to increase by 1 the 'stop' values (since they
-    // are passed by values this only affects this function).
-    internal_ystop = internal_ystop + 1;
-    internal_xstop = internal_xstop + 1;
+    // Fields
+    Kokkos::View<double**> ssha_t_view("ssha_t", internal_xstop+1, internal_ystop+1);
+    Kokkos::View<double**> sshn_t_view("sshn_t", internal_xstop+1, internal_ystop+1);
+    Kokkos::View<double**> sshn_u_view("sshn_u", internal_xstop+1, internal_ystop+1);
+    Kokkos::View<double**> sshn_v_view("sshn_v", internal_xstop+1, internal_ystop+1);
+    Kokkos::View<double**> hu_view("hu", internal_xstop+1, internal_ystop+1);
+    Kokkos::View<double**> hv_view("hv", internal_xstop+1, internal_ystop+1);
+    Kokkos::View<double**> un_view("un", internal_xstop+1, internal_ystop+1);
+    Kokkos::View<double**> vn_view("vn", internal_xstop+1, internal_ystop+1);
+    Kokkos::View<double**> ua_view("ua", internal_xstop+1, internal_ystop+1);
+    Kokkos::View<double**> ht_view("ht", internal_xstop+1, internal_ystop+1);
+    Kokkos::View<double**> ssha_u_view("ssha_u", internal_xstop+1, internal_ystop+1);
+    Kokkos::View<double**> va_view("va", internal_xstop+1, internal_ystop+1);
+    Kokkos::View<double**> ssha_v_view("ssha_v", internal_xstop+1, internal_ystop+1);
+
+    // Grid
+    Kokkos::View<int**> tmask_view("tmask_v", internal_xstop+1, internal_ystop+1);
+    Kokkos::View<double**> area_t_view("area_t", internal_xstop+1, internal_ystop+1);
+    Kokkos::View<double**> area_u_view("area_u", internal_xstop+1, internal_ystop+1);
+    Kokkos::View<double**> area_v_view("area_v", internal_xstop+1, internal_ystop+1);
+    Kokkos::View<double**> dx_u_view("dx_u", internal_xstop+1, internal_ystop+1);
+    Kokkos::View<double**> dx_v_view("dx_v", internal_xstop+1, internal_ystop+1);
+    Kokkos::View<double**> dx_t_view("dx_t", internal_xstop+1, internal_ystop+1);
+    Kokkos::View<double**> dy_u_view("dy_u", internal_xstop+1, internal_ystop+1);
+    Kokkos::View<double**> dy_v_view("dy_v", internal_xstop+1, internal_ystop+1);
+    Kokkos::View<double**> dy_t_view("dy_t", internal_xstop+1, internal_ystop+1);
+    Kokkos::View<double**> gphiu_view("gphiu", internal_xstop+1, internal_ystop+1);
+    Kokkos::View<double**> gphiv_view("gphiv", internal_xstop+1, internal_ystop+1);
+
+    // In this implementation the kernels are manually inlined because:
+    // - Using the kernels/c_family/* , the GPU version created the LAMBDA and then
+    // a call to a gpu function.
+    // - It can use 2D Views (which have a (x,y) notation instead of []).
 
     // Continuity kernel (internal domain)
     Kokkos::parallel_for("continuity",
         mdrange_policy({internal_ystart, internal_xstart},
                        {internal_ystop, internal_xstop}),
         KOKKOS_LAMBDA (const int jj, const int ji) {
-            continuity_code(ji, jj, width, ssha_t, sshn_t, sshn_u, sshn_v, \
-                hu, hv, un, vn, rdt, area_t);
+            double rtmp1, rtmp2, rtmp3, rtmp4;
+
+            rtmp1 = (sshn_u_view(jj, ji)   + hu_view(jj, ji))   * un_view(jj, ji);
+            rtmp2 = (sshn_u_view(jj, ji-1) + hu_view(jj, ji-1)) * un_view(jj, ji-1);
+            rtmp3 = (sshn_v_view(jj, ji)   + hv_view(jj, ji))   * vn_view(jj, ji);
+            rtmp4 = (sshn_v_view(jj-1, ji) + hv_view(jj-1, ji)) * vn_view(jj-1, ji);
+
+            ssha_t_view(jj, ji) = sshn_t_view(jj, ji) + (rtmp2 - rtmp1 + rtmp4 - rtmp3) *
+                rdt / area_t_view(jj, ji);
         }
     );
-
+/*
     // Momentum_u kernel (internal domain u points)
     Kokkos::parallel_for("momentum_u",
         mdrange_policy({internal_ystart, internal_xstart},
                        {internal_ystop, internal_xstop - 1}),
         KOKKOS_LAMBDA (const int jj, const int ji) {
-            momentum_u_code(ji, jj, width, ua, un, vn, hu, hv, ht, ssha_u, \
-                sshn_t, sshn_u, sshn_v, tmask, dx_u, dx_v, dx_t, dy_u, dy_t, \
-                area_u, gphiu, rdt, cbfr, visc, omega, d2r, g);
+            momentum_u_code(ji, jj, width, ua_view.data(), un_view.data(), vn_view.data(),
+                    hu_view.data(), hv_view.data(), ht_view.data(), ssha_u_view.data(), \
+                sshn_t_view.data(), sshn_u_view.data(), sshn_v_view.data(), tmask_view.data(), dx_u_view.data(), dx_v_view.data(), dx_t_view.data(), dy_u_view.data(), dy_t_view.data(), \
+                area_u_view.data(), gphiu_view.data(), rdt, cbfr, visc, omega, d2r, g);
 
         }
     );
@@ -121,9 +153,9 @@ extern "C" void c_invoke_time_step(
         mdrange_policy({internal_ystart, internal_xstart},
                        {internal_ystop - 1, internal_xstop}),
         KOKKOS_LAMBDA (const int jj, const int ji) {
-            momentum_v_code(ji, jj, width, va, un, vn, hu, hv, ht, ssha_v, \
-                sshn_t, sshn_u, sshn_v, tmask, dx_v, dx_t, dy_u, dy_v, dy_t, \
-                area_v, gphiv, rdt, cbfr, visc, omega, d2r, g);
+            momentum_v_code(ji, jj, width, va_view.data(), un_view.data(), vn_view.data(), hu_view.data(), hv_view.data(), ht_view.data(), ssha_v_view.data(), \
+                sshn_t_view.data(), sshn_u_view.data(), sshn_v_view.data(), tmask_view.data(), dx_v_view.data(), dx_t_view.data(), dy_u_view.data(), dy_v_view.data(), dy_t_view.data(), \
+                area_v_view.data(), gphiv_view.data(), rdt, cbfr, visc, omega, d2r, g);
 
         }
     );
@@ -179,9 +211,9 @@ extern "C" void c_invoke_time_step(
                        {internal_ystop + 1, internal_xstop + 1}),
         KOKKOS_LAMBDA (const int jj, const int ji) {
             int idx = jj * width + ji;
-            un[idx] = ua[idx];
-            vn[idx] = va[idx];
-            sshn_t[idx] = ssha_t[idx];
+            un(jj, ji) = ua(jj, ji);
+            vn(jj, ji) = va(jj, ji);
+            sshn_t(jj, ji) = ssha_t(jj, ji);
         }
     );
 
@@ -202,5 +234,5 @@ extern "C" void c_invoke_time_step(
             next_sshv_code(ji, jj, width, sshn_v, sshn_t, tmask, area_t, area_v);
         }
     );
-
+*/
 }
