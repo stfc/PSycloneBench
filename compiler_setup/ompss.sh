fc8077538f6c69519cf81b55b9025f2b82630ac2
Aidan Chalk <aidan.chalk@stfc.ac.uk>
2020-06-29 15:04:46 +0100

    First implementation of C++ ompss code

---
+++ compiler_setup/ompss.sh
@@ -0,0 +1,34 @@
+# Build settings for gfortran compiler
+F90=mpifort
+CC=mpicc
+CXX=mpic++
+
+# Common optimization flags for CFLAGS and F90FLAGS
+OPTFLAGS=" -O3 -mtune=native -finline-limit=50000 -fopt-info-all=gnu_opt_report.txt"
+OPTFLAGS+=" -g"
+
+CFLAGS=$OPTFLAGS
+F90FLAGS="-Wall -Wsurprising -Wuninitialized"
+#F90FLAGS += -O0
+#F90FLAGS += -fcheck=all -fbacktrace -ffpe-trap=invalid -g
+F90FLAGS+=" -faggressive-function-elimination"
+F90FLAGS+=$OPTFLAGS
+# f2py does not break long lines so tell gfortran not to
+# limit the length of a line
+F90FLAGS+=" -ffree-line-length-none"
+
+OMPFLAGS=""
+OMPFLAGS=" -fopenmp"
+
+LDFLAGS=""
+
+AR=ar
+
+export F90
+export F90FLAGS
+export CFLAGS
+export OMPFLAGS
+export LDFLAGS
+export AR
+export CC
+export CXX
