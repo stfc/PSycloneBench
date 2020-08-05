0b3a218cb36674fedcec9c495a8c0e7432fc42c5
Sergi Sisó <sergiesg@gmail.com>
2020-08-05 17:25:46 +0100

    NemoLite2D C OpenCL Tasks: Update Makefile for Xilinx offline compilation of task kernels

--- compiler_setup/xilinx.sh
+++ compiler_setup/xilinx.sh
@@ -1,9 +1,9 @@
 # Environment variables for OpenCL for Xilinx FPGAs
 
-export PLATFORM = xilinx_u200_xdma_201830_2
-export EXECUTION_TARGET = sw_emu # Options: sw_emu | hw_emu | hw
-export OCL_COMPILER = "v++ --compile --platform ${PLATFORM} --target ${EXECUTION_TARGET}"
-export OCL_LINKER = "v++ --link --platform ${PLATFORM} --target ${EXECUTION_TARGET}"
+export PLATFORM=xilinx_u200_xdma_201830_2
+export EXECUTION_TARGET=sw_emu # Options: sw_emu | hw_emu | hw
+export OCL_COMPILER="v++ --compile --platform ${PLATFORM} --target ${EXECUTION_TARGET}"
+export OCL_LINKER="v++ --link --platform ${PLATFORM} --target ${EXECUTION_TARGET}"
 
 # Host compiler flags
 export OPENCL_LIBS="-lOpenCL"
