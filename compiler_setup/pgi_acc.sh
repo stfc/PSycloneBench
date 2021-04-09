6caeee70b8ce6522ce4eee07c0a5bd34882b98a8
Andrew Porter <andrew.porter@stfc.ac.uk>
2021-04-09 10:57:54 +0100

    #68 update NVIDIA compiler flags

--- compiler_setup/pgi_acc.sh
+++ compiler_setup/pgi_acc.sh
@@ -7,6 +7,7 @@ CC=pgcc
 CFLAGS="-g"
 # Fortran compiler flags
 F90FLAGS="-O3 -Minfo=all"
+# Debugging options
 #F90FLAGS"+=" -fcheck=all -fbacktrace -ffpe-trap=invalid -g -O0"
 # -Mcuda is for CUDA Fortran
 # nordc - do not link to routines compiled for device (ensure
@@ -17,14 +18,20 @@ F90FLAGS="-O3 -Minfo=all"
 # shoul be a multiple of 8.
 # -Mcuda is required to build CUDA Fortran
 # For Quadro K600
-F90FLAGS+=" -acc -ta=tesla:cc30,nordc -Mcuda=cc30,nordc"
+#F90FLAGS+=" -acc -ta=tesla:cc30,nordc -Mcuda=cc30,nordc"
 # For Tesla K20c
 #F90FLAGS+=" -acc -ta=tesla,cc35,maxregcount:80,nordc -Mcuda=cc35,maxregcount:80,nordc"
+# Generic flags
+F90FLAGS+=" -acc -ta=tesla:nordc -Mcuda=nordc"
 # Linker flags
 # For Quadro K600
-LDFLAGS+=" -acc -ta=tesla,cc30 -Mcuda=cc30,nordc"
+#LDFLAGS+=" -acc -ta=tesla,cc30 -Mcuda=cc30,nordc"
 # For Tesla K20c
 #LDFLAGS="-acc -ta=nvidia,cc35 -Mcuda=cc35,nordc"
+# Generic flags
+LDFLAGS="-acc -ta=nvidia -Mcuda=nordc"
+# Location of various CUDA maths libraries
+LDFLAGS+=" -L${CUDA_MATH_DIR}/lib64"
 # Flags to use when compiling with OpenMP support
 OMPFLAGS="-mp"
 # Command to use to create archive of object files
