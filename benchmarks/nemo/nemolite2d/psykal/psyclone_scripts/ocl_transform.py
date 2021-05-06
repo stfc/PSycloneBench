97832c29a4697b15e4dd270e3c92df6e1d57c4d0
Sergi Sisó <sergiesg@gmail.com>
2021-05-06 11:21:42 +0100

    Update NemoLite2D OpenCL generation and Xilinx Makefiles

--- benchmarks/nemo/nemolite2d/psykal/psyclone_scripts/ocl_transform.py
+++ benchmarks/nemo/nemolite2d/psykal/psyclone_scripts/ocl_transform.py
@@ -2,31 +2,84 @@
 function via the -s option. Applies OpenCL to the Schedule so
 that PSyclone will generate an OpenCL PSy layer. '''
 
+import os
 from psyclone.psyGen import TransInfo
 from psyclone.domain.gocean.transformations import \
     GOMoveIterationBoundariesInsideKernelTrans
+from psyclone.configuration import Config
 
+FUCTIONAL_PARALLELISM = False
+MOVE_BOUNDARIES = True
+XILINX_CONFIG_FILE = False
+TILING = 64
 
 def trans(psy):
     ''' Transform the schedule for OpenCL generation '''
+
+    # Import transformations
     tinfo = TransInfo()
     globaltrans = tinfo.get_trans_name('KernelGlobalsToArguments')
     move_boundaries_trans = GOMoveIterationBoundariesInsideKernelTrans()
     cltrans = tinfo.get_trans_name('OCLTrans')
 
-    # Get the invoke
+    # Get the invoke routine
     schedule = psy.invokes.get('invoke_0').schedule
+
+    # Map the kernels by their name to different OpenCL queues. The multiple
+    # command queues can be executed concurrently while each command queue
+    # executes in-order its kernels. This provides functional parallelism
+    # when kernels don't have dependencies between them.
+    qmap = {
+        'continuity_code': 1,
+        'momentum_u_code': 2,
+        'momentum_v_code': 3,
+        'bc_ssh_code': 1,
+        'bc_solid_u_code': 2,
+        'bc_solid_v_code': 3,
+        'bc_flather_u_code': 2,
+        'bc_flather_v_code': 3,
+        # Needs a barrier here (currently added manually)
+        'field_copy_code': 1,
+        'next_sshu_code': 1,
+        'next_sshv_code': 1
+        }
 
     # Remove global variables from inside each kernel, pass the boundary
     # values as arguments to the kernel and set the OpenCL work size to 64,
     # which is required for performance (with OpenCL < 1.2 this requires
     # the resulting application to be executed with DL_ESM_ALIGNMENT=64)
     for kern in schedule.kernels():
+        print(kern.name)
         globaltrans.apply(kern)
-        move_boundaries_trans.apply(kern)
-        kern.set_opencl_options({'local_size': 64})
+        if MOVE_BOUNDARIES:
+            move_boundaries_trans.apply(kern)
+        if FUCTIONAL_PARALLELISM:
+            kern.set_opencl_options({'local_size': TILING,
+                                     'queue_number': qmap[kern.name]})
+        else:
+            kern.set_opencl_options({'local_size': TILING})
+
 
     # Transform invoke to OpenCL
     cltrans.apply(schedule)
+
+    if XILINX_CONFIG_FILE:
+        # Create a Xilinx Compiler Configuration file
+        path = Config.get().kernel_output_dir
+        with open(os.path.join(path, "xilinx.cfg"), "w") as cfgfile:
+            cfgfile.write("# Xilinx FPGA configuration file\n")
+            #cfgfile.write("[connectivity]\n")
+            #cfgfile.write("# Create 2 CU of the given kernels\n")
+            #cfgfile.write("nk=continuity_code:2\n")
+            #cfgfile.write("nk=momentum_u_code:2\n")
+            #cfgfile.write("nk=momentum_v_code:2\n")
+            #cfgfile.write("nk=bc_ssh_code:2\n")
+
+            #cfgfile.write("\n[hls]\n")
+            #cfgfile.write("# Assign CUs to different SLRs\n")
+            #cfgfile.write("slr=momentum_u_code_1:SLR0\n")
+            #cfgfile.write("slr=momentum_u_code_2:SLR0\n")
+            #cfgfile.write("slr=momentum_v_code_1:SLR2\n")
+            #cfgfile.write("slr=momentum_v_code_2:SLR2\n")
 
     return psy
--- compiler_setup/xilinx.sh
+++ compiler_setup/xilinx.sh
@@ -1,10 +1,10 @@
 # Environment variables for OpenCL for Xilinx FPGAs
 
-export PLATFORM=xilinx_u200_xdma_201830_2
-export EXECUTION_TARGET=hw # Options: sw_emu | hw_emu | hw
-export XILINX_FLAGS="--jobs 8 -O2 --report_level 1 --config xilinx.cfg"
-export OCL_COMPILER="v++ --compile --platform ${PLATFORM} --target ${EXECUTION_TARGET} ${XILINX_FLAGS}"
-export OCL_LINKER="v++ --link --platform ${PLATFORM} --target ${EXECUTION_TARGET} ${XILINX_FLAGS}"
+export FPGA_PLATFORM=xilinx_u200_xdma_201830_2
+export FPGA_EXECUTION_TARGET=hw # Options: sw_emu | hw_emu | hw
+export OCL_DEVICE_FLAGS="-O2 --report_level 1 --config xilinx.cfg"
+export OCL_COMPILER="v++ --compile"
+export OCL_LINKER="v++ --link"
 
 # Host compiler flags
 export OPENCL_LIBS="-lOpenCL"
