7c8919bd68d293a77b87d36c4d95954cbf458da8
Aidan Chalk <3043914+LonelyCat124@users.noreply.github.com>
2022-01-13 11:19:19 +0000

    #72 Fixing review comments

--- benchmarks/nemo/nemolite2d/psykal/psyclone_scripts/omp_task_transform.py
+++ benchmarks/nemo/nemolite2d/psykal/psyclone_scripts/omp_task_transform.py
@@ -1,8 +1,7 @@
 ''' Python script intended to be passed to PSyclone's generate()
-function via the -s option. It applies OpenMP to every loop and
-inlines all kernels in the schedule.'''
+function via the -s option. It applies OpenMP tasking to every loop
+and inlines all kernels in the schedule.'''
 
-from psyclone.psyGen import TransInfo
 from psyclone.psyir.nodes import Loop
 from psyclone.configuration import Config
 from psyclone.transformations import OMPParallelTrans, OMPSingleTrans, \
@@ -10,7 +9,6 @@ from psyclone.transformations import OMPParallelTrans, OMPSingleTrans, \
 from psyclone.psyir.transformations import OMPTaskwaitTrans
 from psyclone.psyir.nodes import OMPTaskloopDirective, OMPTaskwaitDirective, \
                                  OMPDirective, OMPParallelDirective
-from psyclone.psyGen import HaloExchange
 
 
 def trans(psy):
@@ -19,13 +17,10 @@ def trans(psy):
 
     schedule = psy.invokes.get('invoke_0').schedule
 
-
-
     loop_trans = OMPTaskloopTrans(grainsize=32, nogroup=True)
     wait_trans = OMPTaskwaitTrans()
 
     module_inline_trans = KernelModuleInlineTrans()
-
 
     # Inline all kernels in this Schedule
     for kernel in schedule.kernels():
@@ -38,35 +33,50 @@ def trans(psy):
     single_trans = OMPSingleTrans()
     parallel_trans = OMPParallelTrans()
     sets = []
-    if config.distributed_memory:
-        # Find all of the groupings of taskloop and taskwait directives. Each of these
-        # groups needs its own parallel+single regions.
-        next_start = 0
-        next_end = 0
-        for index in range(len(schedule.children)):
-            child = schedule.children[index]
-            if isinstance(child, OMPTaskloopDirective) or isinstance(child, OMPTaskwaitDirective):
-                next_end = next_end + 1
-            elif not isinstance(child, OMPDirective):
-                if next_start == index:
-                    next_end = index + 1
-                    next_start = index + 1
-                else:
-                    sets.append((next_start, next_end))
-                    next_end = index + 1
-                    next_start = index+1
-            else:
-                next_end = next_end + 1
-        if next_start <= index:
-            sets.append((next_start, index+1))
-        sets.reverse()
-        for next_set in sets:
-            single_trans.apply(schedule[next_set[0]:next_set[1]])
-            parallel_trans.apply(schedule[next_set[0]])
-        for child in schedule.children:
-            if isinstance(child, OMPParallelDirective):
-                wait_trans.apply(child)
-    else:
+    if not config.distributed_memory:
         single_trans.apply(schedule.children)
         parallel_trans.apply(schedule.children)
         wait_trans.apply(schedule.children[0])
+        return
+    # Find all of the groupings of taskloop and taskwait directives. Each of
+    # these groups needs its own parallel+single regions. This makes sure we
+    # don't apply OpenMP transformations to the Halo Exchange operations.
+    next_start = 0
+    next_end = 0
+    for idx in range(len(schedule.children)):
+        # Loop through through the schedule until we find a non-OpenMP
+        # node, and extend the grouping until we do.
+        child = schedule.children[idx]
+        if isinstance(child, (OMPTaskloopDirective, OMPTaskwaitDirective)):
+            next_end = next_end + 1
+        elif not isinstance(child, OMPDirective):
+            # If we find a non OpenMP directive, if we're currently in a
+            # grouping of OpenMP directives then we stop, and add it to
+            # the set of groupings. Otherwise we just skip over this
+            # node.
+            if next_start == idx:
+                next_end = idx + 1
+                next_start = idx + 1
+            else:
+                sets.append((next_start, next_end))
+                next_end = idx + 1
+                next_start = idx+1
+        else:
+            next_end = next_end + 1
+    # If currently in a grouping of directives, add it to the list
+    # of groupings
+    if next_start <= idx:
+        sets.append((next_start, idx+1))
+    # Start from the last grouping to keep indexing correct,
+    # so reverse the ordering
+    sets.reverse()
+    # For each of the groupings of OpenMP directives, surround them
+    # with an OpenMP Single and an OpenMP Parallel directive set.
+    for next_set in sets:
+        single_trans.apply(schedule[next_set[0]:next_set[1]])
+        parallel_trans.apply(schedule[next_set[0]])
+    # Finally, we loop over the OMPParallelDirectives, and apply the
+    # OMPTaskWaitTrans to ensure correctness.
+    for child in schedule.children:
+        if isinstance(child, OMPParallelDirective):
+            wait_trans.apply(child)
