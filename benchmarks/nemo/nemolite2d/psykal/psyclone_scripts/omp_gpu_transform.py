7c4ccf080d4280a74b51f5e66742cc7e828daed6
Sergi Siso <sergi.siso@stfc.ac.uk>
2022-12-20 15:40:14 +0000

    Update NemoLite2D OMP GPU script

--- benchmarks/nemo/nemolite2d/psykal/psyclone_scripts/omp_gpu_transform.py
+++ benchmarks/nemo/nemolite2d/psykal/psyclone_scripts/omp_gpu_transform.py
@@ -3,10 +3,14 @@ function via the -s option. It applies OpenMP to every loop and
 inlines all kernels in the schedule.'''
 
 from psyclone.domain.common.transformations import KernelModuleInlineTrans
+from psyclone.psyir.nodes import Routine, Loop
 from psyclone.psyir.transformations import OMPTargetTrans
 from psyclone.psyir.transformations import OMPLoopTrans
 from psyclone.psyir.transformations import TransformationError
-from psyclone.psyir.nodes import Loop
+from psyclone.psyir.transformations import \
+    FoldConditionalReturnExpressionsTrans
+from psyclone.transformations import \
+    KernelImportsToArguments, OMPDeclareTargetTrans
 
 
 def trans(psy):
@@ -14,14 +18,21 @@ def trans(psy):
     loop_trans = OMPLoopTrans()
     target_trans = OMPTargetTrans()
     module_inline_trans = KernelModuleInlineTrans()
+    imports_to_arguments = KernelImportsToArguments()
+    fold_trans = FoldConditionalReturnExpressionsTrans()
+    omp_declare = OMPDeclareTargetTrans()
 
-    schedule = psy.invokes.get('invoke_0').schedule
+    schedule = psy.invokes.invoke_list[0].schedule
 
     # Inline all kernels in this Schedule
     for kernel in schedule.kernels():
+        imports_to_arguments.apply(kernel)
+        fold_trans.apply(kernel.get_kernel_schedule())
         module_inline_trans.apply(kernel)
+        kernel.lower_to_language_level()
 
     loop_trans.omp_directive = "teamsdistributeparalleldo"
+    loop_trans.omp_schedule = "none"
 
     for outer_loop in schedule.walk(Loop, stop_type=Loop):
         try:
@@ -30,5 +41,9 @@ def trans(psy):
             target_trans.apply(outer_loop.parent.parent)
         except TransformationError:
             pass
+
+    for kernel_subroutine in schedule.parent.walk(Routine):
+        if not kernel_subroutine.name.startswith("invoke_"):
+            omp_declare.apply(kernel_subroutine)
 
     return psy
