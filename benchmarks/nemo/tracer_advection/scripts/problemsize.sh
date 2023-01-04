e03219250ac4407a26a34cc2389da51e66c7ef5a
Andrew Porter <andrew.porter@stfc.ac.uk>
2023-01-04 11:24:52 +0000

    #92 improve problemsize script by adding warm-up runs and removing output file

--- benchmarks/nemo/tracer_advection/scripts/problemsize.sh
+++ benchmarks/nemo/tracer_advection/scripts/problemsize.sh
@@ -16,15 +16,26 @@ export IT=500
 # Number of vertical levels
 export JPK=75
 
+export JPI=128
+export JPJ=128
+taskset -c 2 $@ > /dev/null 2>&1
+
 base=2
 #for power in $(seq 4 12); do
-for power in $(seq 4 9); do
+for power in $(seq 6 10); do
 
     size=$(echo "$base^$power" | bc)
     export JPI=${size}
     export JPJ=${size}
 
+    if (( $power < 6 ));
+    then
+      # Do a warm-up run if it's a small problem size
+      taskset -c 2 $@ > /dev/null 2>&1
+    fi
     time=$(taskset -c 2 $@  | awk '{if ($1 == "Time-stepping") {print $5} }')
 
     echo $size $time
 done
+
+rm -f output.dat
