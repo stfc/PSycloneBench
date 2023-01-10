0b0c36396f98bc67bd8ff75d57f3c8fbe3957775
Aidan Chalk <3043914+LonelyCat124@users.noreply.github.com>
2023-01-10 10:44:43 +0000

    Added updated task transformation

--- benchmarks/nemo/nemolite2d/psykal/psyclone_scripts/omp_taskloop_transform.py
+++ benchmarks/nemo/nemolite2d/psykal/psyclone_scripts/omp_taskloop_transform.py
@@ -4,11 +4,11 @@ and inlines all kernels in the schedule.'''
 
 from psyclone.psyir.nodes import Loop
 from psyclone.configuration import Config
-from psyclone.transformations import OMPParallelTrans, OMPSingleTrans
-from psyclone.domain.common.transformations import KernelModuleInlineTrans
-from psyclone.psyir.transformations import OMPTaskTrans, ChunkLoopTrans
-from psyclone.psyir.nodes import OMPParallelDirective, OMPTaskDirective, \
-                                 OMPDirective
+from psyclone.transformations import OMPParallelTrans, OMPSingleTrans, \
+                                     OMPTaskloopTrans, KernelModuleInlineTrans
+from psyclone.psyir.transformations import OMPTaskwaitTrans
+from psyclone.psyir.nodes import OMPTaskloopDirective, OMPTaskwaitDirective, \
+                                 OMPDirective, OMPParallelDirective
 
 
 def trans(psy):
@@ -17,25 +17,18 @@ def trans(psy):
 
     schedule = psy.invokes.get('invoke_0').schedule
 
-    loop_trans = ChunkLoopTrans()
-    task_trans = OMPTaskTrans()
+    loop_trans = OMPTaskloopTrans(grainsize=32, nogroup=True)
+    wait_trans = OMPTaskwaitTrans()
 
     module_inline_trans = KernelModuleInlineTrans()
 
     # Inline all kernels in this Schedule
-#    for kernel in schedule.kernels():
-#        module_inline_trans.apply(kernel)
+    for kernel in schedule.kernels():
+        module_inline_trans.apply(kernel)
 
-    applications = 0
-
-    for child in schedule.children[:]:
+    for child in schedule.children:
         if isinstance(child, Loop):
             loop_trans.apply(child)
-            assert isinstance(child.children[3].children[0], Loop)
-            task_trans.apply(child)
-            applications = applications + 1
-
-    print(f"Applied task transformation {applications} times.")
 
     single_trans = OMPSingleTrans()
     parallel_trans = OMPParallelTrans()
@@ -43,7 +36,7 @@ def trans(psy):
     if not config.distributed_memory:
         single_trans.apply(schedule.children)
         parallel_trans.apply(schedule.children)
-        print(f"Found {len(schedule.walk(OMPTaskDirective))} task directives.")
+        wait_trans.apply(schedule.children[0])
         return
     # Find all of the groupings of taskloop and taskwait directives. Each of
     # these groups needs its own parallel+single regions. This makes sure we
@@ -54,7 +47,7 @@ def trans(psy):
     for idx, child in enumerate(schedule.children):
         # Loop through through the schedule until we find a non-OpenMP
         # node, and extend the grouping until we do.
-        if isinstance(child, (OMPTaskDirective)):
+        if isinstance(child, (OMPTaskloopDirective, OMPTaskwaitDirective)):
             next_end = next_end + 1
         elif not isinstance(child, OMPDirective):
             # If we find a non OpenMP directive, if we're currently in a
@@ -82,3 +75,8 @@ def trans(psy):
     for next_set in sets:
         single_trans.apply(schedule[next_set[0]:next_set[1]])
         parallel_trans.apply(schedule[next_set[0]])
+    # Finally, we loop over the OMPParallelDirectives, and apply the
+    # OMPTaskWaitTrans to ensure correctness.
+    for child in schedule.children:
+        if isinstance(child, OMPParallelDirective):
+            wait_trans.apply(child)
