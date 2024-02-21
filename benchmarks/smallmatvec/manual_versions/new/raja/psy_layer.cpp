fa98b5324c28745ac430aadb75895cf7470afce5
Sergi Siso <sergi.siso@stfc.ac.uk>
2024-02-21 22:31:50 +0000

    Add Umpire and RAJA launch loops for the linear-kouter

--- benchmarks/smallmatvec/manual_versions/new/raja/psy_layer.cpp
+++ benchmarks/smallmatvec/manual_versions/new/raja/psy_layer.cpp
@@ -3,9 +3,11 @@
 #include <cstring>
 #include "RAJA/RAJA.hpp"
 #include "chai/ManagedArray.hpp"
+#include "umpire/ResourceManager.hpp"
+#include "umpire/strategy/QuickPool.hpp"
 
 #if defined(USE_CUDA_POLICY)
-    using policy = RAJA::cuda_exec<256>;
+    using policy = RAJA::cuda_exec<256, false>;
     using atomic = RAJA::cuda_atomic;
 #elif defined(USE_OPENMP_POLICY)
     using policy = RAJA::omp_parallel_for_exec;   
@@ -99,6 +101,34 @@ extern "C" void c_psy_layer(
     printf("Using RAJA sequential version \n");
 #endif
 
+    auto& rm = umpire::ResourceManager::getInstance();
+    auto pool = rm.makeAllocator<umpire::strategy::QuickPool>("pool", rm.getAllocator("UM"));
+    double * lhs_um = static_cast<double*>(pool.allocate(undf_lhs * sizeof(double)));
+    double * x_um = static_cast<double*>(pool.allocate(undf_x * sizeof(double)));
+    int * map_lhs_um = static_cast<int*>(pool.allocate(ndf_lhs * ncell * sizeof(int)));
+    int * map_x_um = static_cast<int*>(pool.allocate(ndf_x * ncell * sizeof(int)));
+    double * matrix_um = static_cast<double*>(pool.allocate(ndf_lhs * ndf_x * ncell_3d * sizeof(double)));
+    printf("Umpire Pool allocation complete \n");
+
+    RAJA::forall<RAJA::loop_exec>(RAJA::RangeSegment(0, undf_lhs), [=] (int idx) {
+        lhs_um[idx] = lhs[idx];
+    });
+    RAJA::forall<RAJA::loop_exec>(RAJA::RangeSegment(0, ndf_lhs * ncell), [=] (int idx) {
+        map_lhs_um[idx] = map_lhs[idx];
+    });
+    printf("Copy from C array to UM lhs array complete \n");
+    RAJA::forall<RAJA::loop_exec>(RAJA::RangeSegment(0, undf_x), [=] (int idx) {
+        x_um[idx] = x[idx];
+    });
+    RAJA::forall<RAJA::loop_exec>(RAJA::RangeSegment(0, ndf_x * ncell), [=] (int idx) {
+        map_x_um[idx] = map_x[idx];
+    });
+    printf("Copy from C array to UM x array complete \n");
+    RAJA::forall<RAJA::loop_exec>(RAJA::RangeSegment(0, ndf_lhs * ndf_x * ncell_3d), [=] (int idx) {
+        matrix_um[idx] = matrix[idx];
+    });
+    printf("Copy from C array to UM matrix array complete \n");
+
     if (memcmp(traverse,"linear-kinner",11)==0){
         printf("Starting computation with linear and kinner\n");
         for (int iter = 1; iter <= niters; iter ++){
@@ -119,10 +149,49 @@ extern "C" void c_psy_layer(
             //     matrix_vector_code_kouter_atomics(cell, nlayers, lhs, x, ncell_3d, matrix,
             //             ndf_lhs, undf_lhs, &map_lhs[cell*ndf_lhs], ndf_x, undf_x, &map_x[cell*ndf_x]);
             // }
-            RAJA::forall<policy>(RAJA::RangeSegment(0, ncell), [=] RAJA_DEVICE (int cell) {
-                 matrix_vector_code_kouter_atomics(cell, nlayers, lhs, x, ncell_3d, matrix,
-                     ndf_lhs, undf_lhs, &map_lhs[cell*ndf_lhs], ndf_x, undf_x, &map_x[cell*ndf_x]);
-            });
+            // RAJA::forall<policy>(RAJA::RangeSegment(0, ncell), [=] RAJA_DEVICE (int cell) {
+            //      matrix_vector_code_kouter_atomics(cell, nlayers, lhs_um, x_um, ncell_3d, matrix_um,
+            //          ndf_lhs, undf_lhs, &map_lhs_um[cell*ndf_lhs], ndf_x, undf_x, &map_x_um[cell*ndf_x]);
+            // });
+            RAJA::launch<RAJA::LaunchPolicy<RAJA::cuda_launch_t<false>>>( // For CPU: omp_launch_t
+                RAJA::LaunchParams(
+                    RAJA::Teams(ncell),
+                    RAJA::Threads(1,ndf_lhs,nlayers)),
+                    [=] RAJA_HOST_DEVICE (RAJA::LaunchContext ctx) {
+                        // for (int cell=0; cell < ncell; cell ++){
+                        RAJA::loop<RAJA::LoopPolicy<RAJA::cuda_block_x_direct>>(ctx, RAJA::RangeSegment(0, ncell), // For CPU: omp_for
+                            [&] (int cell){
+                                int ik = cell*nlayers;
+                                RAJA::loop<RAJA::LoopPolicy<RAJA::loop_exec>>(ctx, RAJA::RangeSegment(0, ndf_x), // For CPU: omp_for
+                                [&] (int df2){
+                                // for (int df2 = 0; df2 < ndf_x; df2 ++){ // cuda_thread_x_direct, for cpu: loop/sequential
+                                    int m2 = map_x_um[cell*ndf_x + df2] - 1; // -1 because map2 contains fortran 1-indexing references
+                                    RAJA::loop<RAJA::LoopPolicy<RAJA::cuda_thread_y_direct>>(ctx, RAJA::RangeSegment(0, ndf_lhs), // For CPU: omp_for
+                                    [&] (int df){
+                                    // for (int df = 0; df < ndf_lhs; df ++){ //cuda_thread_y_direct
+                                        int m1 = map_lhs_um[cell*ndf_lhs + df] - 1; // -1 because map2 contains fortran 1-indexing references
+                                        RAJA::loop<RAJA::LoopPolicy<RAJA::cuda_thread_z_direct>>(ctx, RAJA::RangeSegment(0, nlayers), // For CPU: omp_for
+                                        [&] (int k){
+                                        // for (int k = 0; k < nlayers; k ++){ // cuda_thread_z_direct
+                                            RAJA::atomicAdd<atomic>(&lhs_um[m1+k], matrix_um[(ik+k)*ndf_lhs*ndf_x + df2*ndf_lhs + df] * x_um[m2+k]);
+                                        });
+                                    });
+                                });
+                            });
+                    });
+            // for (int cell=0; cell < ncell; cell ++){
+            //     int ik = cell*nlayers;
+            //     for (int df2 = 0; df2 < ndf_x; df2 ++){
+            //         int m2 = map_x_um[df2] - 1; // -1 because map2 contains fortran 1-indexing references
+            //         for (int df = 0; df < ndf_lhs; df ++){
+            //             int m1 = map_lhs_um[df] - 1; // -1 because map2 contains fortran 1-indexing references
+            //             for (int k = 0; k < nlayers; k ++){
+            // 		    RAJA::atomicAdd<atomic>(&lhs_um[m1+k], matrix_um[(ik+k)*ndf_lhs*ndf_x + df2*ndf_lhs + df] * x_um[m2+k]);
+            //             }
+            //         }
+            //     }
+            // }
+
         }
     }
 
@@ -160,4 +229,7 @@ extern "C" void c_psy_layer(
             }
         }
     }
+    RAJA::forall<RAJA::loop_exec>(RAJA::RangeSegment(0, undf_lhs), [=] (int idx) {
+        lhs[idx] = lhs_um[idx];
+    });
 }
