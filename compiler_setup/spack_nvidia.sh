401ece2e82b43dde3d5f84be7f2d56fb787f8ea1
Andrew Porter <andrew.porter@stfc.ac.uk>
2025-10-23 12:13:53 +0100

    Modernise compiler flags and fix acc-mixed-umem target

--- compiler_setup/spack_nvidia.sh
+++ compiler_setup/spack_nvidia.sh
@@ -7,5 +7,5 @@ export F90=$FC
 export PSYCLONE_NVIDIA_LIB_DIR=${HOME}/Projects/PSyclone/lib/profiling/nvidia
 export OMPTARGETFLAGS="-mp=gpu -gpu=ccnative"
 export OMPFLAGS="-mp"
-export UMEMFLAGS="-gpu=managed"
+export UMEMFLAGS="-gpu=mem:managed"
 
