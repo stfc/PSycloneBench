d0d6c06aa79ae93d2ece2cf7c78efcf20843aef2
Andrew Porter <andrew.porter@stfc.ac.uk>
2025-10-23 13:12:24 +0100

    Fix other versions of tra_adv benchmark

--- compiler_setup/spack_nvidia.sh
+++ compiler_setup/spack_nvidia.sh
@@ -5,6 +5,7 @@
 # ==============================
 export F90=$FC
 
+export LDFLAGS="-cuda -L${CUDA_HOME}/lib64 -lnvToolsExt"
 export OMPTARGETFLAGS="-mp=gpu -gpu=ccnative"
 export OMPFLAGS="-mp"
 export UMEMFLAGS="-gpu=mem:managed"
