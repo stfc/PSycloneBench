6c5eb135339325895d2524d1c7fc639bf4fb4075
Andrew Porter <andrew.porter@stfc.ac.uk>
2025-10-23 13:00:20 +0100

    Rm path to profiling lib from spack-setup script

--- compiler_setup/spack_nvidia.sh
+++ compiler_setup/spack_nvidia.sh
@@ -4,8 +4,9 @@
 
 # ==============================
 export F90=$FC
-export PSYCLONE_NVIDIA_LIB_DIR=${HOME}/Projects/PSyclone/lib/profiling/nvidia
+
 export OMPTARGETFLAGS="-mp=gpu -gpu=ccnative"
 export OMPFLAGS="-mp"
 export UMEMFLAGS="-gpu=mem:managed"
+export ACCFLAGS="-acc=gpu -gpu=ccnative"
 
