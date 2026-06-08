66a668d8da2f66ab4a1a2e1d86f09f2a9dfb5675
LonelyCat124 <3043914+LonelyCat124@users.noreply.github.com.>
2026-06-08 15:05:29 +0100

    added comments explaining the flags.

--- compiler_setup/amdflang.sh
+++ compiler_setup/amdflang.sh
@@ -22,14 +22,20 @@ F90FLAGS="-O3 -g -fsave-optimization-record -ffast-math"
 # Flags to use when compiling with OpenMP support
 OMPFLAGS="-fopenmp=libomp"
 # Flags to use when compiling with OpenMP GPU offloading support
+# -mp Enables OpenMP generation.
+# -fopenmp-offload-mandatory tells the runtime to always offload OpenMP regions.
+# -fopenmp-force-usm forces unified shared memory between host and device for the whole code.
 OMPTARGETFLAGS="-fopenmp=libomp --offload-arch=gfx942 -mp -fopenmp-offload-mandatory -fopenmp-force-usm"
 
 UMEMFLAGS=" "
 
 # Linker flags
+# -fno-lto disables link time optimisation
+# --ofload-arch=gfx942 targets the AMD MI300A GPU for code generation.
+# -fopenmp-offload-mandatory tells the runtime to always offload OpenMP regions.
+# -fopenmp-force-usm forces unified shared memory between host and device for the whole code.
+# -lflang_rt.hostdevice tells the linker to use the flang host-device runtime. This is not necessary on versions after 22.2.0
 LDFLAGS="-lomp -lomptarget -fopenmp=libomp -fno-lto --offload-arch=gfx942 -fopenmp-offload-mandatory -fopenmp-force-usm -lflang_rt.hostdevice"
-## Location of various CUDA maths libraries
-#LDFLAGS+=" -L${CUDA_MATH_DIR}/lib64"
 
 # Command to use to create archive of object files
 AR=ar
